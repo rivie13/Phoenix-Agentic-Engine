rivie13/Phoenix-Agentic-Engine

commit 69c1ee4629fe1f543d7becdd753ce160a663dcfe

removed duplicated functions in class hierarchy that were bound more than once added a check to detect this case in the future

drivers/unix/stream_peer_tcp_posix.cpp
@@ -139,14 +139,14 @@ void StreamPeerTCPPosix::set_socket(int p_sockfd, IP_Address p_host, int p_port,
 	peer_port = p_port;
 };
 
-Error StreamPeerTCPPosix::connect(const IP_Address& p_host, uint16_t p_port) {
+Error StreamPeerTCPPosix::connect_to_host(const IP_Address& p_host, uint16_t p_port) {
 
 	ERR_FAIL_COND_V( p_host == IP_Address(), ERR_INVALID_PARAMETER);
 
 	sockfd = _socket_create(ip_type, SOCK_STREAM, IPPROTO_TCP);
 	if (sockfd == -1) {
 		ERR_PRINT("Socket creation failed!");
-		disconnect();
+		disconnect_from_host();
 		//perror("socket");
 		return FAILED;
 	};
@@ -165,7 +165,7 @@ Error StreamPeerTCPPosix::connect(const IP_Address& p_host, uint16_t p_port) {
 	if (::connect(sockfd, (struct sockaddr *)&their_addr,addr_size) == -1 && errno != EINPROGRESS) {
 
 		ERR_PRINT("Connection to remote host failed!");
-		disconnect();
+		disconnect_from_host();
 		return FAILED;
 	};
 
@@ -217,8 +217,8 @@ Error StreamPeerTCPPosix::write(const uint8_t* p_data,int p_bytes, int &r_sent, 
 			if (errno != EAGAIN) {
 
 				perror("shit?");
-				disconnect();
-				ERR_PRINT("Server disconnected!\n");
+				disconnect_from_host();
+				ERR_PRINT("Server disconnect_from_hosted!\n");
 				return FAILED;
 			};
 
@@ -243,7 +243,7 @@ Error StreamPeerTCPPosix::write(const uint8_t* p_data,int p_bytes, int &r_sent, 
 
 Error StreamPeerTCPPosix::read(uint8_t* p_buffer, int p_bytes,int &r_received, bool p_block) {
 
-	if (!is_connected()) {
+	if (!is_connected_to_host()) {
 
 		return FAILED;
 	};
@@ -274,8 +274,8 @@ Error StreamPeerTCPPosix::read(uint8_t* p_buffer, int p_bytes,int &r_received, b
 			if (errno != EAGAIN) {
 
 				perror("shit?");
-				disconnect();
-				ERR_PRINT("Server disconnected!\n");
+				disconnect_from_host();
+				ERR_PRINT("Server disconnect_from_hosted!\n");
 				return FAILED;
 			};
 
@@ -308,12 +308,12 @@ Error StreamPeerTCPPosix::read(uint8_t* p_buffer, int p_bytes,int &r_received, b
 
 void StreamPeerTCPPosix::set_nodelay(bool p_enabled) {
 
-	ERR_FAIL_COND(!is_connected());
+	ERR_FAIL_COND(!is_connected_to_host());
 	int flag=p_enabled?1:0;
 	setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(int));
 }
 
-bool StreamPeerTCPPosix::is_connected() const {
+bool StreamPeerTCPPosix::is_connected_to_host() const {
 
 	if (status == STATUS_NONE || status == STATUS_ERROR) {
 
@@ -336,7 +336,7 @@ StreamPeerTCP::Status StreamPeerTCPPosix::get_status() const {
 };
 
 
-void StreamPeerTCPPosix::disconnect() {
+void StreamPeerTCPPosix::disconnect_from_host() {
 
 	if (sockfd != -1)
 		close(sockfd);
@@ -398,7 +398,7 @@ StreamPeerTCPPosix::StreamPeerTCPPosix() {
 
 StreamPeerTCPPosix::~StreamPeerTCPPosix() {
 
-	disconnect();
+	disconnect_from_host();
 };
 
 #endif

platform/windows/stream_peer_winsock.cpp
@@ -149,8 +149,8 @@ Error StreamPeerWinsock::write(const uint8_t* p_data,int p_bytes, int &r_sent, b
 			if (WSAGetLastError() != WSAEWOULDBLOCK) {
 
 				perror("shit?");
-				disconnect();
-				ERR_PRINT("Server disconnected!\n");
+				disconnect_from_host();
+				ERR_PRINT("Server disconnect_from_hosted!\n");
 				return FAILED;
 			};
 
@@ -175,7 +175,7 @@ Error StreamPeerWinsock::write(const uint8_t* p_data,int p_bytes, int &r_sent, b
 
 Error StreamPeerWinsock::read(uint8_t* p_buffer, int p_bytes,int &r_received, bool p_block) {
 
-	if (!is_connected()) {
+	if (!is_connected_to_host()) {
 
 		return FAILED;
 	};
@@ -206,8 +206,8 @@ Error StreamPeerWinsock::read(uint8_t* p_buffer, int p_bytes,int &r_received, bo
 			if (WSAGetLastError() != WSAEWOULDBLOCK) {
 
 				perror("shit?");
-				disconnect();
-				ERR_PRINT("Server disconnected!\n");
+				disconnect_from_host();
+				ERR_PRINT("Server disconnect_from_hosted!\n");
 				return FAILED;
 			};
 
@@ -218,7 +218,7 @@ Error StreamPeerWinsock::read(uint8_t* p_buffer, int p_bytes,int &r_received, bo
 			};
 			_block(sockfd, true, false);
 		} else if (read == 0) {
-			disconnect();
+			disconnect_from_host();
 			return ERR_FILE_EOF;
 		} else {
 
@@ -264,7 +264,7 @@ StreamPeerTCP::Status StreamPeerWinsock::get_status() const {
 };
 
 
-bool StreamPeerWinsock::is_connected() const {
+bool StreamPeerWinsock::is_connected_to_host() const {
 
 	if (status == STATUS_NONE || status == STATUS_ERROR) {
 
@@ -277,7 +277,7 @@ bool StreamPeerWinsock::is_connected() const {
 	return (sockfd!=INVALID_SOCKET);
 };
 
-void StreamPeerWinsock::disconnect() {
+void StreamPeerWinsock::disconnect_from_host() {
 
 	if (sockfd != INVALID_SOCKET)
 		closesocket(sockfd);
@@ -298,14 +298,14 @@ void StreamPeerWinsock::set_socket(int p_sockfd, IP_Address p_host, int p_port, 
 	peer_port = p_port;
 };
 
-Error StreamPeerWinsock::connect(const IP_Address& p_host, uint16_t p_port) {
+Error StreamPeerWinsock::connect_to_host(const IP_Address& p_host, uint16_t p_port) {
 
 	ERR_FAIL_COND_V( p_host == IP_Address(), ERR_INVALID_PARAMETER);
 
 	sockfd = _socket_create(ip_type, SOCK_STREAM, IPPROTO_TCP);
 	if (sockfd  == INVALID_SOCKET) {
 		ERR_PRINT("Socket creation failed!");
-		disconnect();
+		disconnect_from_host();
 		//perror("socket");
 		return FAILED;
 	};
@@ -313,7 +313,7 @@ Error StreamPeerWinsock::connect(const IP_Address& p_host, uint16_t p_port) {
 	unsigned long par = 1;
 	if (ioctlsocket(sockfd, FIONBIO, &par)) {
 		perror("setting non-block mode");
-		disconnect();
+		disconnect_from_host();
 		return FAILED;
 	};
 
@@ -324,7 +324,7 @@ Error StreamPeerWinsock::connect(const IP_Address& p_host, uint16_t p_port) {
 
 		if (WSAGetLastError() != WSAEWOULDBLOCK) {
 			ERR_PRINT("Connection to remote host failed!");
-			disconnect();
+			disconnect_from_host();
 			return FAILED;
 		};
 		status = STATUS_CONNECTING;
@@ -339,7 +339,7 @@ Error StreamPeerWinsock::connect(const IP_Address& p_host, uint16_t p_port) {
 };
 
 void StreamPeerWinsock::set_nodelay(bool p_enabled) {
-    ERR_FAIL_COND(!is_connected());
+    ERR_FAIL_COND(!is_connected_to_host());
     int flag=p_enabled?1:0;
     setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(int));
 }
@@ -373,7 +373,7 @@ StreamPeerWinsock::StreamPeerWinsock() {
 
 StreamPeerWinsock::~StreamPeerWinsock() {
 
-	disconnect();
+	disconnect_from_host();
 };
 
 

platform/windows/stream_peer_winsock.h
@@ -58,7 +58,7 @@ protected:
 
 public:
 
-	virtual Error connect(const IP_Address& p_host, uint16_t p_port);
+	virtual Error connect_to_host(const IP_Address& p_host, uint16_t p_port);
 
 	virtual Error put_data(const uint8_t* p_data,int p_bytes);
 	virtual Error put_partial_data(const uint8_t* p_data,int p_bytes, int &r_sent);

core/bind/core_bind.cpp
@@ -2508,22 +2508,22 @@ void _ClassDB::_bind_methods() {
 	ClassDB::bind_method(_MD("can_instance","class"),&_ClassDB::can_instance);
 	ClassDB::bind_method(_MD("instance","class"),&_ClassDB::instance);
 
-	ClassDB::bind_method(_MD("has_signal","class","signal"),&_ClassDB::has_signal);
-	ClassDB::bind_method(_MD("get_signal","class","signal"),&_ClassDB::get_signal);
-	ClassDB::bind_method(_MD("get_signal_list","class","no_inheritance"),&_ClassDB::get_signal_list,DEFVAL(false));
+	ClassDB::bind_method(_MD("class_has_signal","class","signal"),&_ClassDB::has_signal);
+	ClassDB::bind_method(_MD("class_get_signal","class","signal"),&_ClassDB::get_signal);
+	ClassDB::bind_method(_MD("class_get_signal_list","class","no_inheritance"),&_ClassDB::get_signal_list,DEFVAL(false));
 
-	ClassDB::bind_method(_MD("get_property_list","class","no_inheritance"),&_ClassDB::get_property_list,DEFVAL(false));
+	ClassDB::bind_method(_MD("class_get_property_list","class","no_inheritance"),&_ClassDB::get_property_list,DEFVAL(false));
 
-	ClassDB::bind_method(_MD("has_method","class","method","no_inheritance"),&_ClassDB::has_method,DEFVAL(false));
+	ClassDB::bind_method(_MD("class_has_method","class","method","no_inheritance"),&_ClassDB::has_method,DEFVAL(false));
 
-	ClassDB::bind_method(_MD("get_method_list","class","no_inheritance"),&_ClassDB::get_method_list,DEFVAL(false));
+	ClassDB::bind_method(_MD("class_get_method_list","class","no_inheritance"),&_ClassDB::get_method_list,DEFVAL(false));
 
-	ClassDB::bind_method(_MD("get_integer_constant_list","class","no_inheritance"),&_ClassDB::get_integer_constant_list,DEFVAL(false));
+	ClassDB::bind_method(_MD("class_get_integer_constant_list","class","no_inheritance"),&_ClassDB::get_integer_constant_list,DEFVAL(false));
 
-	ClassDB::bind_method(_MD("has_integer_constant","class","name"),&_ClassDB::has_integer_constant);
-	ClassDB::bind_method(_MD("get_integer_constant","class","name"),&_ClassDB::get_integer_constant);
+	ClassDB::bind_method(_MD("class_has_integer_constant","class","name"),&_ClassDB::has_integer_constant);
+	ClassDB::bind_method(_MD("class_get_integer_constant","class","name"),&_ClassDB::get_integer_constant);
 
-	ClassDB::bind_method(_MD("get_category","class"),&_ClassDB::get_category);
+	ClassDB::bind_method(_MD("class_get_category","class"),&_ClassDB::get_category);
 	ClassDB::bind_method(_MD("is_class_enabled","class"),&_ClassDB::is_class_enabled);
 
 

core/io/file_access_network.cpp
@@ -206,7 +206,7 @@ Error FileAccessNetworkClient::connect(const String& p_host,int p_port,const Str
 	}
 
 	DEBUG_PRINT("IP: "+String(ip)+" port "+itos(p_port));
-	Error err = client->connect(ip,p_port);
+	Error err = client->connect_to_host(ip,p_port);
 	ERR_FAIL_COND_V(err,err);
 	while(client->get_status()==StreamPeerTCP::STATUS_CONNECTING) {
 //DEBUG_PRINT("trying to connect....");

core/io/http_client.cpp
@@ -33,7 +33,7 @@ void HTTPClient::set_ip_type(IP::Type p_type) {
 	ip_type = p_type;
 }
 
-Error HTTPClient::connect(const String &p_host, int p_port, bool p_ssl,bool p_verify_host){
+Error HTTPClient::connect_to_host(const String &p_host, int p_port, bool p_ssl,bool p_verify_host){
 
 	close();
 	tcp_connection->set_ip_type(ip_type);
@@ -57,7 +57,7 @@ Error HTTPClient::connect(const String &p_host, int p_port, bool p_ssl,bool p_ve
 
 	if (conn_host.is_valid_ip_address()) {
 		//is ip
-		Error err = tcp_connection->connect(IP_Address(conn_host),p_port);
+		Error err = tcp_connection->connect_to_host(IP_Address(conn_host),p_port);
 		if (err) {
 			status=STATUS_CANT_CONNECT;
 			return err;
@@ -232,7 +232,7 @@ Error HTTPClient::get_response_headers(List<String> *r_response) {
 void HTTPClient::close(){
 
 	if (tcp_connection->get_status()!=StreamPeerTCP::STATUS_NONE)
-		tcp_connection->disconnect();
+		tcp_connection->disconnect_from_host();
 
 	connection.unref();
 	status=STATUS_DISCONNECTED;
@@ -267,7 +267,7 @@ Error HTTPClient::poll(){
 				case IP::RESOLVER_STATUS_DONE: {
 
 					IP_Address host = IP::get_singleton()->get_resolve_item_address(resolving);
-					Error err = tcp_connection->connect(host,conn_port);
+					Error err = tcp_connection->connect_to_host(host,conn_port);
 					IP::get_singleton()->erase_resolve_item(resolving);
 					resolving=IP::RESOLVER_INVALID_ID;
 					if (err) {
@@ -300,7 +300,7 @@ Error HTTPClient::poll(){
 				case StreamPeerTCP::STATUS_CONNECTED: {
 					if (ssl) {
 						Ref<StreamPeerSSL> ssl = StreamPeerSSL::create();
-						Error err = ssl->connect(tcp_connection,true,ssl_verify_host?conn_host:String());
+						Error err = ssl->connect_to_stream(tcp_connection,true,ssl_verify_host?conn_host:String());
 						if (err!=OK) {
 							close();
 							status=STATUS_SSL_HANDSHAKE_ERROR;
@@ -640,7 +640,7 @@ Error HTTPClient::_get_http_data(uint8_t* p_buffer, int p_bytes,int &r_received)
 void HTTPClient::_bind_methods() {
 
 	ClassDB::bind_method(_MD("set_ip_type","ip_type"),&HTTPClient::set_ip_type);
-	ClassDB::bind_method(_MD("connect:Error","host","port","use_ssl","verify_host"),&HTTPClient::connect,DEFVAL(false),DEFVAL(true));
+	ClassDB::bind_method(_MD("connect_to_host:Error","host","port","use_ssl","verify_host"),&HTTPClient::connect_to_host,DEFVAL(false),DEFVAL(true));
 	ClassDB::bind_method(_MD("set_connection","connection:StreamPeer"),&HTTPClient::set_connection);
 	ClassDB::bind_method(_MD("get_connection:StreamPeer"),&HTTPClient::get_connection);
 	ClassDB::bind_method(_MD("request_raw","method","url","headers","body"),&HTTPClient::request_raw);

core/io/http_client.h
@@ -167,7 +167,7 @@ public:
 
 	void set_ip_type(IP::Type p_type);
 	//Error connect_and_get(const String& p_url,bool p_verify_host=true); //connects to a full url and perform request
-	Error connect(const String &p_host,int p_port,bool p_ssl=false,bool p_verify_host=true);
+	Error connect_to_host(const String &p_host,int p_port,bool p_ssl=false,bool p_verify_host=true);
 
 	void set_connection(const Ref<StreamPeer>& p_connection);
 	Ref<StreamPeer> get_connection() const;

core/io/stream_peer_ssl.cpp
@@ -57,10 +57,10 @@ bool StreamPeerSSL::is_available() {
 void StreamPeerSSL::_bind_methods() {
 
 
-	ClassDB::bind_method(_MD("accept:Error","stream:StreamPeer"),&StreamPeerSSL::accept);
-	ClassDB::bind_method(_MD("connect:Error","stream:StreamPeer","validate_certs","for_hostname"),&StreamPeerSSL::connect,DEFVAL(false),DEFVAL(String()));
+	ClassDB::bind_method(_MD("accept_stream:Error","stream:StreamPeer"),&StreamPeerSSL::accept_stream);
+	ClassDB::bind_method(_MD("connect_to_stream:Error","stream:StreamPeer","validate_certs","for_hostname"),&StreamPeerSSL::connect_to_stream,DEFVAL(false),DEFVAL(String()));
 	ClassDB::bind_method(_MD("get_status"),&StreamPeerSSL::get_status);
-	ClassDB::bind_method(_MD("disconnect"),&StreamPeerSSL::disconnect);
+	ClassDB::bind_method(_MD("disconnect_from_stream"),&StreamPeerSSL::disconnect_from_stream);
 	BIND_CONSTANT( STATUS_DISCONNECTED );
 	BIND_CONSTANT( STATUS_CONNECTED );
 	BIND_CONSTANT( STATUS_ERROR_NO_CERTIFICATE );

commit 3bfe8143573f48ccdb5817df00a3cc550fae3d1d

StreamPeerWinsock: Fix changed declarations
Bug introduced in 69c1ee4629fe1f543d7becdd753ce160a663dcfe.

drivers/unix/stream_peer_tcp_posix.cpp
@@ -218,7 +218,7 @@ Error StreamPeerTCPPosix::write(const uint8_t* p_data,int p_bytes, int &r_sent, 
 
 				perror("shit?");
 				disconnect_from_host();
-				ERR_PRINT("Server disconnect_from_hosted!\n");
+				ERR_PRINT("Server disconnected!\n");
 				return FAILED;
 			};
 
@@ -275,7 +275,7 @@ Error StreamPeerTCPPosix::read(uint8_t* p_buffer, int p_bytes,int &r_received, b
 
 				perror("shit?");
 				disconnect_from_host();
-				ERR_PRINT("Server disconnect_from_hosted!\n");
+				ERR_PRINT("Server disconnected!\n");
 				return FAILED;
 			};
 

platform/windows/stream_peer_winsock.cpp
@@ -150,7 +150,7 @@ Error StreamPeerWinsock::write(const uint8_t* p_data,int p_bytes, int &r_sent, b
 
 				perror("shit?");
 				disconnect_from_host();
-				ERR_PRINT("Server disconnect_from_hosted!\n");
+				ERR_PRINT("Server disconnected!\n");
 				return FAILED;
 			};
 
@@ -207,7 +207,7 @@ Error StreamPeerWinsock::read(uint8_t* p_buffer, int p_bytes,int &r_received, bo
 
 				perror("shit?");
 				disconnect_from_host();
-				ERR_PRINT("Server disconnect_from_hosted!\n");
+				ERR_PRINT("Server disconnected!\n");
 				return FAILED;
 			};
 

platform/windows/stream_peer_winsock.h
@@ -73,9 +73,9 @@ public:
 	virtual IP_Address get_connected_host() const;
 	virtual uint16_t get_connected_port() const;
 
-	virtual bool is_connected() const;
+	virtual bool is_connected_to_host() const;
 	virtual Status get_status() const;
-	virtual void disconnect();
+	virtual void disconnect_from_host();
 
 	static void make_default();
 	static void cleanup();