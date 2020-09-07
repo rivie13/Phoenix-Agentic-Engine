rivie13/Phoenix-Agentic-Engine

commit 0696f2ae7eeacdda789a45529509ab7d086dc85a

Fix emoji branch compilation error
(cherry picked from commit 2407562366213b16f1242ab997827a9e45c76a25)

methods.py
@@ -86,7 +86,7 @@ def update_version(module_version_string=""):
             gitfolder = module_folder[8:]
 
     if os.path.isfile(os.path.join(gitfolder, "HEAD")):
-        head = open(os.path.join(gitfolder, "HEAD"), "r").readline().strip()
+        head = open(os.path.join(gitfolder, "HEAD"), "r", encoding="utf8").readline().strip()
         if head.startswith("ref: "):
             head = os.path.join(gitfolder, head[5:])
             if os.path.isfile(head):

commit 0a47e275896271f3b2ec7557114a43eacde5d09a

SCons: Fix regression in Python 2 support
Introduced in 0696f2ae7eeacdda789a45529509ab7d086dc85a by not using
the compat method.

Fixes #41858.

methods.py
@@ -3,7 +3,7 @@ import re
 import glob
 import subprocess
 from collections import OrderedDict
-from compat import iteritems, isbasestring, decode_utf8, qualname
+from compat import iteritems, isbasestring, open_utf8, decode_utf8, qualname
 
 
 def add_source_files(self, sources, files, warn_duplicates=True):
@@ -86,7 +86,7 @@ def update_version(module_version_string=""):
             gitfolder = module_folder[8:]
 
     if os.path.isfile(os.path.join(gitfolder, "HEAD")):
-        head = open(os.path.join(gitfolder, "HEAD"), "r", encoding="utf8").readline().strip()
+        head = open_utf8(os.path.join(gitfolder, "HEAD"), "r").readline().strip()
         if head.startswith("ref: "):
             head = os.path.join(gitfolder, head[5:])
             if os.path.isfile(head):