KleirRampage45/redot-engine

Setting visibility on GridMap now works. Closes #41374.
Continuing the work from 791992119c5692ad178d1209cc96f89e4c7c195e,
It seems the maps visibility was not actually set and a flase value was propogated to the meshes
Trying to set the maps visibility directly (Using `set_visibility` causes the map to no longer to recieve visibility notifications, instead this approach was chosen

modules/gridmap/grid_map.cpp
@@ -706,7 +706,7 @@ void GridMap::_update_visibility() {
 		Octant *octant = e->value();
 		for (int i = 0; i < octant->multimesh_instances.size(); i++) {
 			const Octant::MultimeshInstance &mi = octant->multimesh_instances[i];
-			RS::get_singleton()->instance_set_visible(mi.instance, is_visible());
+			RS::get_singleton()->instance_set_visible(mi.instance, is_visible_in_tree());
 		}
 	}
 }

rivie13/Phoenix-Agentic-Engine

(cherry picked from commit fe52c6b0b7e21bbed1aaabe684714a9b78c23ba3)

modules/gridmap/grid_map.cpp
@@ -735,7 +735,7 @@ void GridMap::_update_visibility() {
 		Octant *octant = e->value();
 		for (int i = 0; i < octant->multimesh_instances.size(); i++) {
 			const Octant::MultimeshInstance &mi = octant->multimesh_instances[i];
-			VS::get_singleton()->instance_set_visible(mi.instance, is_visible());
+			VS::get_singleton()->instance_set_visible(mi.instance, is_visible_in_tree());
 		}
 	}
 }