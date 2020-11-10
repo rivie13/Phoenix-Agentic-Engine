naithar/godot

iOS Modules: separate main platform code from modules
Moved previously builtin modules 'GameCenter', 'AppStore', 'iCloud' to separate modules to be represented as plugin.
Modified 'ARKit' and 'Camera' to not be builtin into engine and work as plugin.
Changed platform code so it's not affected by the move.
Modified Xcode project file to remove parameters that doesn't make any effect.
Added basic '.gdip' plugin config file.

modules/arkit/arkit_module.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  game_center.h                                                        */
+/*  arkit_module.cpp                                                     */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,49 +28,18 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifdef GAME_CENTER_ENABLED
+#include "arkit_module.h"
 
-#ifndef GAME_CENTER_H
-#define GAME_CENTER_H
+#include "arkit_interface.h"
 
-#include "core/object.h"
+void register_arkit_types() {
+	// does it make sense to register the class?
 
-class GameCenter : public Object {
+	Ref<ARKitInterface> arkit_interface;
+	arkit_interface.instance();
+	ARVRServer::get_singleton()->add_interface(arkit_interface);
+}
 
-	GDCLASS(GameCenter, Object);
-
-	static GameCenter *instance;
-	static void _bind_methods();
-
-	List<Variant> pending_events;
-
-	bool authenticated;
-
-	void return_connect_error(const char *p_error_description);
-
-public:
-	Error authenticate();
-	bool is_authenticated();
-
-	Error post_score(Variant p_score);
-	Error award_achievement(Variant p_params);
-	void reset_achievements();
-	void request_achievements();
-	void request_achievement_descriptions();
-	Error show_game_center(Variant p_params);
-	Error request_identity_verification_signature();
-
-	void game_center_closed();
-
-	int get_pending_event_count();
-	Variant pop_pending_event();
-
-	static GameCenter *get_singleton();
-
-	GameCenter();
-	~GameCenter();
-};
-
-#endif
-
-#endif
+void unregister_arkit_types() {
+	// should clean itself up nicely :)
+}

modules/gamecenter/game_center.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  view_controller.h                                                    */
+/*  game_center.h                                                        */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,18 +28,45 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#import <UIKit/UIKit.h>
+#ifndef GAME_CENTER_H
+#define GAME_CENTER_H
 
-@class GodotView;
-@class GodotNativeVideoView;
+#include "core/object.h"
 
-@interface ViewController : UIViewController
+class GameCenter : public Object {
 
-@property(nonatomic, readonly, strong) GodotView *godotView;
-@property(nonatomic, readonly, strong) GodotNativeVideoView *videoView;
+	GDCLASS(GameCenter, Object);
 
-// MARK: Native Video Player
+	static GameCenter *instance;
+	static void _bind_methods();
 
-- (BOOL)playVideoAtPath:(NSString *)filePath volume:(float)videoVolume audio:(NSString *)audioTrack subtitle:(NSString *)subtitleTrack;
+	List<Variant> pending_events;
 
-@end
+	bool authenticated;
+
+	void return_connect_error(const char *p_error_description);
+
+public:
+	Error authenticate();
+	bool is_authenticated();
+
+	Error post_score(Variant p_score);
+	Error award_achievement(Variant p_params);
+	void reset_achievements();
+	void request_achievements();
+	void request_achievement_descriptions();
+	Error show_game_center(Variant p_params);
+	Error request_identity_verification_signature();
+
+	void game_center_closed();
+
+	int get_pending_event_count();
+	Variant pop_pending_event();
+
+	static GameCenter *get_singleton();
+
+	GameCenter();
+	~GameCenter();
+};
+
+#endif

modules/inappstore/in_app_store.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  view_controller.h                                                    */
+/*  in_app_store.h                                                       */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,18 +28,51 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#import <UIKit/UIKit.h>
+#ifndef IN_APP_STORE_H
+#define IN_APP_STORE_H
 
-@class GodotView;
-@class GodotNativeVideoView;
+#include "core/object.h"
 
-@interface ViewController : UIViewController
+#ifdef __OBJC__
+@class GodotProductsDelegate;
+@class GodotTransactionsObserver;
 
-@property(nonatomic, readonly, strong) GodotView *godotView;
-@property(nonatomic, readonly, strong) GodotNativeVideoView *videoView;
+typedef GodotProductsDelegate InAppStoreProductDelegate;
+typedef GodotTransactionsObserver InAppStoreTransactionObserver;
+#else
+typedef void InAppStoreProductDelegate;
+typedef void InAppStoreTransactionObserver;
+#endif
 
-// MARK: Native Video Player
+class InAppStore : public Object {
 
-- (BOOL)playVideoAtPath:(NSString *)filePath volume:(float)videoVolume audio:(NSString *)audioTrack subtitle:(NSString *)subtitleTrack;
+	GDCLASS(InAppStore, Object);
 
-@end
+	static InAppStore *instance;
+	static void _bind_methods();
+
+	List<Variant> pending_events;
+
+	InAppStoreProductDelegate *products_request_delegate;
+	InAppStoreTransactionObserver *transactions_observer;
+
+public:
+	Error request_product_info(Variant p_params);
+	Error restore_purchases();
+	Error purchase(Variant p_params);
+
+	int get_pending_event_count();
+	Variant pop_pending_event();
+	void finish_transaction(String product_id);
+	void set_auto_finish_transaction(bool b);
+
+	void _post_event(Variant p_event);
+	void _record_purchase(String product_id);
+
+	static InAppStore *get_singleton();
+
+	InAppStore();
+	~InAppStore();
+};
+
+#endif

rivie13/Phoenix-Agentic-Engine

modules/arkit/arkit_module.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  view_controller.h                                                    */
+/*  arkit_module.cpp                                                     */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,18 +28,18 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#import <UIKit/UIKit.h>
+#include "arkit_module.h"
 
-@class GodotView;
-@class GodotNativeVideoView;
+#include "arkit_interface.h"
 
-@interface ViewController : UIViewController
+void register_arkit_types() {
+	// does it make sense to register the class?
 
-@property(nonatomic, readonly, strong) GodotView *godotView;
-@property(nonatomic, readonly, strong) GodotNativeVideoView *videoView;
+	Ref<ARKitInterface> arkit_interface;
+	arkit_interface.instance();
+	XRServer::get_singleton()->add_interface(arkit_interface);
+}
 
-// MARK: Native Video Player
-
-- (BOOL)playVideoAtPath:(NSString *)filePath volume:(float)videoVolume audio:(NSString *)audioTrack subtitle:(NSString *)subtitleTrack;
-
-@end
+void unregister_arkit_types() {
+	// should clean itself up nicely :)
+}

modules/gamecenter/game_center.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  view_controller.h                                                    */
+/*  game_center.h                                                        */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,18 +28,44 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#import <UIKit/UIKit.h>
+#ifndef GAME_CENTER_H
+#define GAME_CENTER_H
 
-@class GodotView;
-@class GodotNativeVideoView;
+#include "core/object/class_db.h"
 
-@interface ViewController : UIViewController
+class GameCenter : public Object {
+	GDCLASS(GameCenter, Object);
 
-@property(nonatomic, readonly, strong) GodotView *godotView;
-@property(nonatomic, readonly, strong) GodotNativeVideoView *videoView;
+	static GameCenter *instance;
+	static void _bind_methods();
 
-// MARK: Native Video Player
+	List<Variant> pending_events;
 
-- (BOOL)playVideoAtPath:(NSString *)filePath volume:(float)videoVolume audio:(NSString *)audioTrack subtitle:(NSString *)subtitleTrack;
+	bool authenticated;
 
-@end
+	void return_connect_error(const char *p_error_description);
+
+public:
+	Error authenticate();
+	bool is_authenticated();
+
+	Error post_score(Dictionary p_score);
+	Error award_achievement(Dictionary p_params);
+	void reset_achievements();
+	void request_achievements();
+	void request_achievement_descriptions();
+	Error show_game_center(Dictionary p_params);
+	Error request_identity_verification_signature();
+
+	void game_center_closed();
+
+	int get_pending_event_count();
+	Variant pop_pending_event();
+
+	static GameCenter *get_singleton();
+
+	GameCenter();
+	~GameCenter();
+};
+
+#endif

modules/inappstore/in_app_store.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  view_controller.h                                                    */
+/*  in_app_store.h                                                       */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,18 +28,50 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#import <UIKit/UIKit.h>
+#ifndef IN_APP_STORE_H
+#define IN_APP_STORE_H
 
-@class GodotView;
-@class GodotNativeVideoView;
+#include "core/object/class_db.h"
 
-@interface ViewController : UIViewController
+#ifdef __OBJC__
+@class GodotProductsDelegate;
+@class GodotTransactionsObserver;
 
-@property(nonatomic, readonly, strong) GodotView *godotView;
-@property(nonatomic, readonly, strong) GodotNativeVideoView *videoView;
+typedef GodotProductsDelegate InAppStoreProductDelegate;
+typedef GodotTransactionsObserver InAppStoreTransactionObserver;
+#else
+typedef void InAppStoreProductDelegate;
+typedef void InAppStoreTransactionObserver;
+#endif
 
-// MARK: Native Video Player
+class InAppStore : public Object {
+	GDCLASS(InAppStore, Object);
 
-- (BOOL)playVideoAtPath:(NSString *)filePath volume:(float)videoVolume audio:(NSString *)audioTrack subtitle:(NSString *)subtitleTrack;
+	static InAppStore *instance;
+	static void _bind_methods();
 
-@end
+	List<Variant> pending_events;
+
+	InAppStoreProductDelegate *products_request_delegate;
+	InAppStoreTransactionObserver *transactions_observer;
+
+public:
+	Error request_product_info(Dictionary p_params);
+	Error restore_purchases();
+	Error purchase(Dictionary p_params);
+
+	int get_pending_event_count();
+	Variant pop_pending_event();
+	void finish_transaction(String product_id);
+	void set_auto_finish_transaction(bool b);
+
+	void _post_event(Variant p_event);
+	void _record_purchase(String product_id);
+
+	static InAppStore *get_singleton();
+
+	InAppStore();
+	~InAppStore();
+};
+
+#endif