MohammadKhashashneh/godot

Add Node processing and physics processing cumulative (as opposed to delta) time.

scene/main/node.cpp
@@ -54,13 +54,17 @@ void Node::_notification(int p_notification) {
 	switch (p_notification) {
 		case NOTIFICATION_PROCESS: {
 			if (get_script_instance()) {
-				Variant time = get_process_delta_time();
+				float _time = get_process_delta_time();
+				Variant time = _time;
+				data.process_cumulative_time += _time;
 				get_script_instance()->call(SceneStringNames::get_singleton()->_process, time);
 			}
 		} break;
 		case NOTIFICATION_PHYSICS_PROCESS: {
 			if (get_script_instance()) {
-				Variant time = get_physics_process_delta_time();
+				float _time = get_physics_process_delta_time();
+				Variant time = _time;
+				data.physics_process_cumulative_time += _time;
 				get_script_instance()->call(SceneStringNames::get_singleton()->_physics_process, time);
 			}
 
@@ -843,6 +847,22 @@ float Node::get_physics_process_delta_time() const {
 	}
 }
 
+float Node::get_physics_process_cumulative_time() const {
+	if (data.tree) {
+		return data.physics_process_cumulative_time;
+	} else {
+		return 0;
+	}
+}
+
+float Node::get_physics_process_total_time() const {
+	if (data.tree) {
+		return data.tree->get_physics_total_time();
+	} else {
+		return 0;
+	}
+}
+
 float Node::get_process_delta_time() const {
 	if (data.tree) {
 		return data.tree->get_process_time();
@@ -869,6 +889,22 @@ void Node::set_process(bool p_process) {
 
 bool Node::is_processing() const {
 	return data.process;
+}
+
+float Node::get_process_cumulative_time() const {
+	if (data.tree) {
+		return data.process_cumulative_time;
+	} else {
+		return 0;
+	}
+}
+
+float Node::get_process_total_time() const {
+	if (data.tree) {
+		return data.tree->get_process_total_time();
+	} else {
+		return 0;
+	}
 }
 
 void Node::set_process_internal(bool p_process_internal) {
@@ -2722,8 +2758,12 @@ void Node::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("propagate_call", "method", "args", "parent_first"), &Node::propagate_call, DEFVAL(Array()), DEFVAL(false));
 	ClassDB::bind_method(D_METHOD("set_physics_process", "enable"), &Node::set_physics_process);
 	ClassDB::bind_method(D_METHOD("get_physics_process_delta_time"), &Node::get_physics_process_delta_time);
+	ClassDB::bind_method(D_METHOD("get_physics_process_cumulative_time"), &Node::get_physics_process_cumulative_time);
+	ClassDB::bind_method(D_METHOD("get_physics_process_total_time"), &Node::get_physics_process_total_time);
 	ClassDB::bind_method(D_METHOD("is_physics_processing"), &Node::is_physics_processing);
 	ClassDB::bind_method(D_METHOD("get_process_delta_time"), &Node::get_process_delta_time);
+	ClassDB::bind_method(D_METHOD("get_process_cumulative_time"), &Node::get_process_cumulative_time);
+	ClassDB::bind_method(D_METHOD("get_process_total_time"), &Node::get_process_total_time);
 	ClassDB::bind_method(D_METHOD("set_process", "enable"), &Node::set_process);
 	ClassDB::bind_method(D_METHOD("set_process_priority", "priority"), &Node::set_process_priority);
 	ClassDB::bind_method(D_METHOD("get_process_priority"), &Node::get_process_priority);

scene/main/node.h
@@ -116,6 +116,9 @@ private:
 		Vector<NetData> rpc_methods;
 		Vector<NetData> rpc_properties;
 
+		float process_cumulative_time = 1.0f;
+		float physics_process_cumulative_time = 1.0f;
+
 		// Variables used to properly sort the node when processing, ignored otherwise.
 		// TODO: Should move all the stuff below to bits.
 		bool physics_process = false;
@@ -334,10 +337,14 @@ public:
 	/* PROCESSING */
 	void set_physics_process(bool p_process);
 	float get_physics_process_delta_time() const;
+	float get_physics_process_cumulative_time() const;
+	float get_physics_process_total_time() const;
 	bool is_physics_processing() const;
 
 	void set_process(bool p_process);
 	float get_process_delta_time() const;
+	float get_process_cumulative_time() const;
+	float get_process_total_time() const;
 	bool is_processing() const;
 
 	void set_physics_process_internal(bool p_process_internal);

scene/main/scene_tree.h
@@ -88,7 +88,9 @@ private:
 
 	uint64_t tree_version = 1;
 	float physics_process_time = 1.0;
+	float physics_total_time = 1.0;
 	float process_time = 1.0;
+	float process_total_time = 1.0;
 	bool accept_quit = true;
 	bool quit_on_go_back = true;
 
@@ -249,7 +251,9 @@ public:
 	void quit(int p_exit_code = -1);
 
 	_FORCE_INLINE_ float get_physics_process_time() const { return physics_process_time; }
+	_FORCE_INLINE_ float get_physics_total_time() const { return physics_total_time; }
 	_FORCE_INLINE_ float get_process_time() const { return process_time; }
+	_FORCE_INLINE_ float get_process_total_time() const { return process_total_time; }
 
 #ifdef TOOLS_ENABLED
 	bool is_node_being_edited(const Node *p_node) const;

rivie13/Phoenix-Agentic-Engine

scene/main/node.cpp
@@ -55,13 +55,17 @@ void Node::_notification(int p_notification) {
 	switch (p_notification) {
 		case NOTIFICATION_PROCESS: {
 			if (get_script_instance()) {
-				Variant time = get_process_delta_time();
+				double d_time = get_process_delta_time();
+				data.process_cumulative_time += d_time;
+				Variant time = d_time;
 				get_script_instance()->call(SceneStringNames::get_singleton()->_process, time);
 			}
 		} break;
 		case NOTIFICATION_PHYSICS_PROCESS: {
 			if (get_script_instance()) {
-				Variant time = get_physics_process_delta_time();
+				double d_time = get_physics_process_delta_time();
+				data.physics_process_cumulative_time += d_time;
+				Variant time = d_time;
 				get_script_instance()->call(SceneStringNames::get_singleton()->_physics_process, time);
 			}
 
@@ -720,6 +724,22 @@ double Node::get_physics_process_delta_time() const {
 	}
 }
 
+double Node::get_physics_process_cumulative_time() const {
+	if (data.tree) {
+		return data.physics_process_cumulative_time;
+	} else {
+		return 0;
+	}
+}
+
+double Node::get_physics_process_total_time() const {
+	if (data.tree) {
+		return data.tree->get_physics_total_time();
+	} else {
+		return 0;
+	}
+}
+
 double Node::get_process_delta_time() const {
 	if (data.tree) {
 		return data.tree->get_process_time();
@@ -744,6 +764,22 @@ void Node::set_process(bool p_process) {
 
 bool Node::is_processing() const {
 	return data.process;
+}
+
+double Node::get_process_cumulative_time() const {
+	if (data.tree) {
+		return data.process_cumulative_time;
+	} else {
+		return 0;
+	}
+}
+
+double Node::get_process_total_time() const {
+	if (data.tree) {
+		return data.tree->get_process_total_time();
+	} else {
+		return 0;
+	}
 }
 
 void Node::set_process_internal(bool p_process_internal) {
@@ -2591,8 +2627,12 @@ void Node::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("propagate_call", "method", "args", "parent_first"), &Node::propagate_call, DEFVAL(Array()), DEFVAL(false));
 	ClassDB::bind_method(D_METHOD("set_physics_process", "enable"), &Node::set_physics_process);
 	ClassDB::bind_method(D_METHOD("get_physics_process_delta_time"), &Node::get_physics_process_delta_time);
+	ClassDB::bind_method(D_METHOD("get_physics_process_cumulative_time"), &Node::get_physics_process_cumulative_time);
+	ClassDB::bind_method(D_METHOD("get_physics_process_total_time"), &Node::get_physics_process_total_time);
 	ClassDB::bind_method(D_METHOD("is_physics_processing"), &Node::is_physics_processing);
 	ClassDB::bind_method(D_METHOD("get_process_delta_time"), &Node::get_process_delta_time);
+	ClassDB::bind_method(D_METHOD("get_process_cumulative_time"), &Node::get_process_cumulative_time);
+	ClassDB::bind_method(D_METHOD("get_process_total_time"), &Node::get_process_total_time);
 	ClassDB::bind_method(D_METHOD("set_process", "enable"), &Node::set_process);
 	ClassDB::bind_method(D_METHOD("set_process_priority", "priority"), &Node::set_process_priority);
 	ClassDB::bind_method(D_METHOD("get_process_priority"), &Node::get_process_priority);

scene/main/node.h
@@ -122,6 +122,9 @@ private:
 		int network_master = 1; // Server by default.
 		Vector<MultiplayerAPI::RPCConfig> rpc_methods;
 
+		double process_cumulative_time = 0.0;
+		double physics_process_cumulative_time = 0.0;
+
 		// Variables used to properly sort the node when processing, ignored otherwise.
 		// TODO: Should move all the stuff below to bits.
 		bool physics_process = false;
@@ -341,10 +344,14 @@ public:
 	/* PROCESSING */
 	void set_physics_process(bool p_process);
 	double get_physics_process_delta_time() const;
+	double get_physics_process_cumulative_time() const;
+	double get_physics_process_total_time() const;
 	bool is_physics_processing() const;
 
 	void set_process(bool p_process);
 	double get_process_delta_time() const;
+	double get_process_cumulative_time() const;
+	double get_process_total_time() const;
 	bool is_processing() const;
 
 	void set_physics_process_internal(bool p_process_internal);

scene/main/scene_tree.h
@@ -92,7 +92,10 @@ private:
 
 	uint64_t tree_version = 1;
 	double physics_process_time = 1.0;
+	double physics_total_time = 0.0;
+	double process_total_time = 0.0;
 	double process_time = 1.0;
+
 	bool accept_quit = true;
 	bool quit_on_go_back = true;
 
@@ -248,7 +251,9 @@ public:
 	void quit(int p_exit_code = EXIT_SUCCESS);
 
 	_FORCE_INLINE_ double get_physics_process_time() const { return physics_process_time; }
+	_FORCE_INLINE_ double get_physics_total_time() const { return physics_total_time; }
 	_FORCE_INLINE_ double get_process_time() const { return process_time; }
+	_FORCE_INLINE_ double get_process_total_time() const { return process_total_time; }
 
 #ifdef TOOLS_ENABLED
 	bool is_node_being_edited(const Node *p_node) const;