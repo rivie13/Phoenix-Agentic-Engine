KleirRampage45/redot-engine

Fix editor saving blank scenes (for real)

editor/editor_node.cpp
@@ -1791,7 +1791,7 @@ void EditorNode::_save_all_scenes() {
 				} else {
 					_save_scene_with_preview(scene->get_scene_file_path());
 				}
-			} else {
+			} else if (scene->get_scene_file_path() != "") {
 				all_saved = false;
 			}
 		}

rivie13/Phoenix-Agentic-Engine

(cherry picked from commit 8144ac79cfb07d4a5e5c906550de9352df68efd3)

editor/editor_node.cpp
@@ -1621,7 +1621,7 @@ void EditorNode::_save_all_scenes() {
 				} else {
 					_save_scene_with_preview(scene->get_filename());
 				}
-			} else {
+			} else if (scene->get_filename() != "") {
 				all_saved = false;
 			}
 		}