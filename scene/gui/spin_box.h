LinoBigatti/godot

Independent spinbox arrow step precision
Rebased by EIREXE

This work has been kindly sponsored by IMVU & EIRTeam.

scene/gui/spin_box.h
@@ -52,6 +52,7 @@ class SpinBox : public Range {
 
 	String prefix;
 	String suffix;
+	double arrow_step = 0.0;
 
 	void _line_edit_input(const Ref<InputEvent> &p_event);
 
@@ -95,6 +96,8 @@ public:
 	bool get_update_on_text_changed() const;
 
 	void apply();
+	void set_arrow_step(const double p_arrow_step);
+	double get_arrow_step() const;
 
 	SpinBox();
 };

rivie13/Phoenix-Agentic-Engine

scene/gui/spin_box.h
@@ -52,6 +52,7 @@ class SpinBox : public Range {
 
 	String prefix;
 	String suffix;
+	double custom_arrow_step = 0.0;
 
 	void _line_edit_input(const Ref<InputEvent> &p_event);
 
@@ -95,6 +96,8 @@ public:
 	bool get_update_on_text_changed() const;
 
 	void apply();
+	void set_custom_arrow_step(const double p_custom_arrow_step);
+	double get_custom_arrow_step() const;
 
 	SpinBox();
 };