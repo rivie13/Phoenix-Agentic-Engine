virtual-puppet-project/godot

C#: Add source generator for method list

modules/mono/glue/GodotSharp/GodotSharp/Core/Bridge/ManagedCallbacks.cs
@@ -24,7 +24,7 @@ namespace Godot.Bridge
         public delegate* unmanaged<godot_string*, godot_ref*, void> ScriptManagerBridge_GetOrCreateScriptBridgeForPath;
         public delegate* unmanaged<IntPtr, void> ScriptManagerBridge_RemoveScriptBridge;
         public delegate* unmanaged<IntPtr, godot_bool> ScriptManagerBridge_TryReloadRegisteredScriptWithClass;
-        public delegate* unmanaged<IntPtr, godot_bool*, godot_dictionary*, godot_dictionary*, godot_ref*, void> ScriptManagerBridge_UpdateScriptClassInfo;
+        public delegate* unmanaged<IntPtr, godot_bool*, godot_dictionary*, godot_dictionary*, godot_dictionary*, godot_ref*, void> ScriptManagerBridge_UpdateScriptClassInfo;
         public delegate* unmanaged<IntPtr, IntPtr*, godot_bool, godot_bool> ScriptManagerBridge_SwapGCHandleForType;
         public delegate* unmanaged<IntPtr, delegate* unmanaged<IntPtr, void*, int, void>, void> ScriptManagerBridge_GetPropertyInfoList;
         public delegate* unmanaged<IntPtr, delegate* unmanaged<IntPtr, void*, int, void>, void> ScriptManagerBridge_GetPropertyDefaultValues;

modules/mono/glue/GodotSharp/GodotSharp/Core/Bridge/ScriptManagerBridge.cs
@@ -554,7 +554,8 @@ namespace Godot.Bridge
 
         [UnmanagedCallersOnly]
         internal static unsafe void UpdateScriptClassInfo(IntPtr scriptPtr, godot_bool* outTool,
-            godot_dictionary* outRpcFunctionsDest, godot_dictionary* outEventSignalsDest, godot_ref* outBaseScript)
+            godot_dictionary* outMethodsDest, godot_dictionary* outRpcFunctionsDest,
+            godot_dictionary* outEventSignalsDest, godot_ref* outBaseScript)
         {
             try
             {
@@ -575,6 +576,47 @@ namespace Godot.Bridge
                 if (!(*outTool).ToBool() && scriptType.Assembly.GetName().Name == "GodotTools")
                     *outTool = godot_bool.True;
 
+                // Methods
+
+                // Performance is not critical here as this will be replaced with source generators.
+                using var methods = new Collections.Dictionary();
+
+                Type? top = scriptType;
+                Type native = Object.InternalGetClassNativeBase(top);
+
+                while (top != null && top != native)
+                {
+                    var methodList = GetMethodListForType(top);
+
+                    if (methodList != null)
+                    {
+                        foreach (var method in methodList)
+                        {
+                            var methodParams = new Collections.Array();
+
+                            if (method.Arguments != null)
+                            {
+                                foreach (var param in method.Arguments)
+                                {
+                                    methodParams.Add(new Collections.Dictionary()
+                                    {
+                                        { "name", param.Name },
+                                        { "type", param.Type },
+                                        { "usage", param.Usage }
+                                    });
+                                }
+                            }
+
+                            methods.Add(method.Name, methodParams);
+                        }
+                    }
+
+                    top = top.BaseType;
+                }
+
+                *outMethodsDest = NativeFuncs.godotsharp_dictionary_new_copy(
+                    (godot_dictionary)methods.NativeValue);
+
                 // RPC functions
 
                 static RPCMode MemberGetRpcMode(MemberInfo memberInfo)
@@ -592,8 +634,7 @@ namespace Godot.Bridge
 
                 Collections.Dictionary<string, Collections.Dictionary> rpcFunctions = new();
 
-                Type? top = scriptType;
-                Type native = Object.InternalGetClassNativeBase(top);
+                top = scriptType;
 
                 while (top != null && top != native)
                 {
@@ -625,9 +666,8 @@ namespace Godot.Bridge
                     top = top.BaseType;
                 }
 
-                *outRpcFunctionsDest =
-                    NativeFuncs.godotsharp_dictionary_new_copy(
-                        (godot_dictionary)((Collections.Dictionary)rpcFunctions).NativeValue);
+                *outRpcFunctionsDest = NativeFuncs.godotsharp_dictionary_new_copy(
+                    (godot_dictionary)((Collections.Dictionary)rpcFunctions).NativeValue);
 
                 // Event signals
 
@@ -666,8 +706,8 @@ namespace Godot.Bridge
                     top = top.BaseType;
                 }
 
-                *outEventSignalsDest =
-                    NativeFuncs.godotsharp_dictionary_new_copy((godot_dictionary)signals.NativeValue);
+                *outEventSignalsDest = NativeFuncs.godotsharp_dictionary_new_copy(
+                    (godot_dictionary)signals.NativeValue);
 
                 // Base script
 
@@ -702,6 +742,19 @@ namespace Godot.Bridge
                 return null;
 
             return (List<MethodInfo>?)getGodotSignalListMethod.Invoke(null, null);
+        }
+
+        private static List<MethodInfo>? GetMethodListForType(Type type)
+        {
+            var getGodotMethodListMethod = type.GetMethod(
+                "GetGodotMethodList",
+                BindingFlags.DeclaredOnly | BindingFlags.Static |
+                BindingFlags.NonPublic | BindingFlags.Public);
+
+            if (getGodotMethodListMethod == null)
+                return null;
+
+            return (List<MethodInfo>?)getGodotMethodListMethod.Invoke(null, null);
         }
 
         // ReSharper disable once InconsistentNaming

modules/mono/mono_gd/gd_mono_cache.h
@@ -88,7 +88,7 @@ struct ManagedCallbacks {
 	using FuncScriptManagerBridge_GetOrCreateScriptBridgeForPath = void(GD_CLR_STDCALL *)(const String *, Ref<CSharpScript> *);
 	using FuncScriptManagerBridge_RemoveScriptBridge = void(GD_CLR_STDCALL *)(const CSharpScript *);
 	using FuncScriptManagerBridge_TryReloadRegisteredScriptWithClass = bool(GD_CLR_STDCALL *)(const CSharpScript *);
-	using FuncScriptManagerBridge_UpdateScriptClassInfo = void(GD_CLR_STDCALL *)(const CSharpScript *, bool *, Dictionary *, Dictionary *, Ref<CSharpScript> *);
+	using FuncScriptManagerBridge_UpdateScriptClassInfo = void(GD_CLR_STDCALL *)(const CSharpScript *, bool *, Dictionary *, Dictionary *, Dictionary *, Ref<CSharpScript> *);
 	using FuncScriptManagerBridge_SwapGCHandleForType = bool(GD_CLR_STDCALL *)(GCHandleIntPtr, GCHandleIntPtr *, bool);
 	using FuncScriptManagerBridge_GetPropertyInfoList = void(GD_CLR_STDCALL *)(CSharpScript *, Callback_ScriptManagerBridge_GetPropertyInfoList_Add);
 	using FuncScriptManagerBridge_GetPropertyDefaultValues = void(GD_CLR_STDCALL *)(CSharpScript *, Callback_ScriptManagerBridge_GetPropertyDefaultValues_Add);

rivie13/Phoenix-Agentic-Engine

modules/mono/glue/GodotSharp/GodotSharp/Core/Bridge/ManagedCallbacks.cs
@@ -24,7 +24,7 @@ namespace Godot.Bridge
         public delegate* unmanaged<godot_string*, godot_ref*, void> ScriptManagerBridge_GetOrCreateScriptBridgeForPath;
         public delegate* unmanaged<IntPtr, void> ScriptManagerBridge_RemoveScriptBridge;
         public delegate* unmanaged<IntPtr, godot_bool> ScriptManagerBridge_TryReloadRegisteredScriptWithClass;
-        public delegate* unmanaged<IntPtr, godot_bool*, godot_dictionary*, godot_dictionary*, godot_ref*, void> ScriptManagerBridge_UpdateScriptClassInfo;
+        public delegate* unmanaged<IntPtr, godot_bool*, godot_array*, godot_dictionary*, godot_dictionary*, godot_ref*, void> ScriptManagerBridge_UpdateScriptClassInfo;
         public delegate* unmanaged<IntPtr, IntPtr*, godot_bool, godot_bool> ScriptManagerBridge_SwapGCHandleForType;
         public delegate* unmanaged<IntPtr, delegate* unmanaged<IntPtr, godot_string*, void*, int, void>, void> ScriptManagerBridge_GetPropertyInfoList;
         public delegate* unmanaged<IntPtr, delegate* unmanaged<IntPtr, void*, int, void>, void> ScriptManagerBridge_GetPropertyDefaultValues;

modules/mono/glue/GodotSharp/GodotSharp/Core/Bridge/ScriptManagerBridge.cs
@@ -557,7 +557,8 @@ namespace Godot.Bridge
 
         [UnmanagedCallersOnly]
         internal static unsafe void UpdateScriptClassInfo(IntPtr scriptPtr, godot_bool* outTool,
-            godot_dictionary* outRpcFunctionsDest, godot_dictionary* outEventSignalsDest, godot_ref* outBaseScript)
+            godot_array* outMethodsDest, godot_dictionary* outRpcFunctionsDest,
+            godot_dictionary* outEventSignalsDest, godot_ref* outBaseScript)
         {
             try
             {
@@ -578,12 +579,58 @@ namespace Godot.Bridge
                 if (!(*outTool).ToBool() && scriptType.Assembly.GetName().Name == "GodotTools")
                     *outTool = godot_bool.True;
 
+                // Methods
+
+                // Performance is not critical here as this will be replaced with source generators.
+                using var methods = new Collections.Array();
+
+                Type? top = scriptType;
+                Type native = Object.InternalGetClassNativeBase(top);
+
+                while (top != null && top != native)
+                {
+                    var methodList = GetMethodListForType(top);
+
+                    if (methodList != null)
+                    {
+                        foreach (var method in methodList)
+                        {
+                            var methodInfo = new Collections.Dictionary();
+
+                            methodInfo.Add("name", method.Name);
+
+                            var methodParams = new Collections.Array();
+
+                            if (method.Arguments != null)
+                            {
+                                foreach (var param in method.Arguments)
+                                {
+                                    methodParams.Add(new Collections.Dictionary()
+                                    {
+                                        { "name", param.Name },
+                                        { "type", param.Type },
+                                        { "usage", param.Usage }
+                                    });
+                                }
+                            }
+
+                            methodInfo.Add("params", methodParams);
+
+                            methods.Add(methodInfo);
+                        }
+                    }
+
+                    top = top.BaseType;
+                }
+
+                *outMethodsDest = NativeFuncs.godotsharp_array_new_copy(
+                    (godot_array)methods.NativeValue);
+
                 // RPC functions
 
                 Collections.Dictionary<string, Collections.Dictionary> rpcFunctions = new();
 
-                Type? top = scriptType;
-                Type native = Object.InternalGetClassNativeBase(top);
+                top = scriptType;
 
                 while (top != null && top != native)
                 {
@@ -617,9 +664,8 @@ namespace Godot.Bridge
                     top = top.BaseType;
                 }
 
-                *outRpcFunctionsDest =
-                    NativeFuncs.godotsharp_dictionary_new_copy(
-                        (godot_dictionary)((Collections.Dictionary)rpcFunctions).NativeValue);
+                *outRpcFunctionsDest = NativeFuncs.godotsharp_dictionary_new_copy(
+                    (godot_dictionary)((Collections.Dictionary)rpcFunctions).NativeValue);
 
                 // Event signals
 
@@ -663,8 +709,8 @@ namespace Godot.Bridge
                     top = top.BaseType;
                 }
 
-                *outEventSignalsDest =
-                    NativeFuncs.godotsharp_dictionary_new_copy((godot_dictionary)signals.NativeValue);
+                *outEventSignalsDest = NativeFuncs.godotsharp_dictionary_new_copy(
+                    (godot_dictionary)signals.NativeValue);
 
                 // Base script
 
@@ -699,6 +745,19 @@ namespace Godot.Bridge
                 return null;
 
             return (List<MethodInfo>?)getGodotSignalListMethod.Invoke(null, null);
+        }
+
+        private static List<MethodInfo>? GetMethodListForType(Type type)
+        {
+            var getGodotMethodListMethod = type.GetMethod(
+                "GetGodotMethodList",
+                BindingFlags.DeclaredOnly | BindingFlags.Static |
+                BindingFlags.NonPublic | BindingFlags.Public);
+
+            if (getGodotMethodListMethod == null)
+                return null;
+
+            return (List<MethodInfo>?)getGodotMethodListMethod.Invoke(null, null);
         }
 
         // ReSharper disable once InconsistentNaming

modules/mono/mono_gd/gd_mono_cache.h
@@ -87,7 +87,7 @@ struct ManagedCallbacks {
 	using FuncScriptManagerBridge_GetOrCreateScriptBridgeForPath = void(GD_CLR_STDCALL *)(const String *, Ref<CSharpScript> *);
 	using FuncScriptManagerBridge_RemoveScriptBridge = void(GD_CLR_STDCALL *)(const CSharpScript *);
 	using FuncScriptManagerBridge_TryReloadRegisteredScriptWithClass = bool(GD_CLR_STDCALL *)(const CSharpScript *);
-	using FuncScriptManagerBridge_UpdateScriptClassInfo = void(GD_CLR_STDCALL *)(const CSharpScript *, bool *, Dictionary *, Dictionary *, Ref<CSharpScript> *);
+	using FuncScriptManagerBridge_UpdateScriptClassInfo = void(GD_CLR_STDCALL *)(const CSharpScript *, bool *, Array *, Dictionary *, Dictionary *, Ref<CSharpScript> *);
 	using FuncScriptManagerBridge_SwapGCHandleForType = bool(GD_CLR_STDCALL *)(GCHandleIntPtr, GCHandleIntPtr *, bool);
 	using FuncScriptManagerBridge_GetPropertyInfoList = void(GD_CLR_STDCALL *)(CSharpScript *, Callback_ScriptManagerBridge_GetPropertyInfoList_Add);
 	using FuncScriptManagerBridge_GetPropertyDefaultValues = void(GD_CLR_STDCALL *)(CSharpScript *, Callback_ScriptManagerBridge_GetPropertyDefaultValues_Add);