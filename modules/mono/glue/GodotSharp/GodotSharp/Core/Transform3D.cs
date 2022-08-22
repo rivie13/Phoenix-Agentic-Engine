godotengine/godot

C#: Code cleanup and greatly reduce use of C# pointers

modules/mono/glue/GodotSharp/GodotSharp/Core/Transform3D.cs
@@ -51,7 +51,7 @@ namespace Godot
                     case 3:
                         return origin;
                     default:
-                        throw new IndexOutOfRangeException();
+                        throw new ArgumentOutOfRangeException(nameof(column));
                 }
             }
             set
@@ -71,7 +71,7 @@ namespace Godot
                         origin = value;
                         return;
                     default:
-                        throw new IndexOutOfRangeException();
+                        throw new ArgumentOutOfRangeException(nameof(column));
                 }
             }
         }
@@ -163,7 +163,7 @@ namespace Godot
         /// <param name="target">The object to look at.</param>
         /// <param name="up">The relative up direction</param>
         /// <returns>The resulting transform.</returns>
-        public Transform3D LookingAt(Vector3 target, Vector3 up)
+        public readonly Transform3D LookingAt(Vector3 target, Vector3 up)
         {
             var t = this;
             t.SetLookAt(origin, target, up);
@@ -187,7 +187,7 @@ namespace Godot
         /// <param name="axis">The axis to rotate around. Must be normalized.</param>
         /// <param name="phi">The angle to rotate, in radians.</param>
         /// <returns>The rotated transformation matrix.</returns>
-        public Transform3D Rotated(Vector3 axis, real_t phi)
+        public readonly Transform3D Rotated(Vector3 axis, real_t phi)
         {
             return new Transform3D(new Basis(axis, phi), new Vector3()) * this;
         }
@@ -360,7 +360,7 @@ namespace Godot
             return !left.Equals(right);
         }
 
-        public override bool Equals(object obj)
+        public override readonly bool Equals(object obj)
         {
             if (obj is Transform3D)
             {
@@ -370,7 +370,7 @@ namespace Godot
             return false;
         }
 
-        public bool Equals(Transform3D other)
+        public readonly bool Equals(Transform3D other)
         {
             return basis.Equals(other.basis) && origin.Equals(other.origin);
         }

rivie13/Phoenix-Agentic-Engine

modules/mono/glue/GodotSharp/GodotSharp/Core/Transform3D.cs
@@ -52,7 +52,7 @@ namespace Godot
                     case 3:
                         return origin;
                     default:
-                        throw new IndexOutOfRangeException();
+                        throw new ArgumentOutOfRangeException(nameof(column));
                 }
             }
             set
@@ -72,7 +72,7 @@ namespace Godot
                         origin = value;
                         return;
                     default:
-                        throw new IndexOutOfRangeException();
+                        throw new ArgumentOutOfRangeException(nameof(column));
                 }
             }
         }
@@ -168,7 +168,7 @@ namespace Godot
         /// <param name="target">The object to look at.</param>
         /// <param name="up">The relative up direction.</param>
         /// <returns>The resulting transform.</returns>
-        public Transform3D LookingAt(Vector3 target, Vector3 up)
+        public readonly Transform3D LookingAt(Vector3 target, Vector3 up)
         {
             Transform3D t = this;
             t.SetLookAt(origin, target, up);
@@ -194,7 +194,7 @@ namespace Godot
         /// <param name="axis">The axis to rotate around. Must be normalized.</param>
         /// <param name="angle">The angle to rotate, in radians.</param>
         /// <returns>The rotated transformation matrix.</returns>
-        public Transform3D Rotated(Vector3 axis, real_t angle)
+        public readonly Transform3D Rotated(Vector3 axis, real_t angle)
         {
             return new Transform3D(new Basis(axis, angle), new Vector3()) * this;
         }
@@ -443,7 +443,7 @@ namespace Godot
         /// </summary>
         /// <param name="obj">The object to compare with.</param>
         /// <returns>Whether or not the transform and the object are exactly equal.</returns>
-        public override bool Equals(object obj)
+        public override readonly bool Equals(object obj)
         {
             if (obj is Transform3D)
             {
@@ -460,7 +460,7 @@ namespace Godot
         /// </summary>
         /// <param name="other">The other transform to compare.</param>
         /// <returns>Whether or not the matrices are exactly equal.</returns>
-        public bool Equals(Transform3D other)
+        public readonly bool Equals(Transform3D other)
         {
             return basis.Equals(other.basis) && origin.Equals(other.origin);
         }