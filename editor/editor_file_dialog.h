godotengine/godot

Port remaining connections to callable_mp

editor/editor_file_dialog.h
@@ -168,7 +168,6 @@ private:
 
 	void _select_drive(int p_idx);
 	void _dir_entered(String p_dir);
-	void _file_entered(const String &p_file);
 	void _action_pressed();
 	void _save_confirm_pressed();
 	void _cancel_pressed();
@@ -202,6 +201,8 @@ protected:
 	static void _bind_methods();
 	//bind helpers
 public:
+	void _file_entered(const String &p_file);
+
 	void popup_file_dialog();
 	void clear_filters();
 	void add_filter(const String &p_filter);

rivie13/Phoenix-Agentic-Engine

editor/editor_file_dialog.h
@@ -202,7 +202,6 @@ private:
 
 	void _select_drive(int p_idx);
 	void _dir_submitted(String p_dir);
-	void _file_submitted(const String &p_file);
 	void _action_pressed();
 	void _save_confirm_pressed();
 	void _cancel_pressed();
@@ -240,6 +239,9 @@ protected:
 	static void _bind_methods();
 
 public:
+	// Public for use with callable_mp.
+	void _file_submitted(const String &p_file);
+
 	void popup_file_dialog();
 	void clear_filters();
 	void add_filter(const String &p_filter, const String &p_description = "");