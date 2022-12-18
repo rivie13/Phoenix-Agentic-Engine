KleirRampage45/redot-engine

Add vector value linking
Co-authored-by: redlamp <244062+redlamp@users.noreply.github.com>

scene/gui/control.cpp
@@ -478,6 +478,10 @@ void Control::_validate_property(PropertyInfo &property) const {
 		}
 	}
 
+	if (property.name == "scale") {
+		property.hint = PROPERTY_HINT_LINK;
+	}
+
 	// Validate which positioning properties should be displayed depending on the parent and the layout mode.
 	Node *parent_node = get_parent_control();
 	if (!parent_node) {

rivie13/Phoenix-Agentic-Engine

(cherry picked from commit 5553e27fe8346eed22ca4a47728fedcf2a7d7ffd)

scene/gui/control.cpp
@@ -432,6 +432,10 @@ void Control::_validate_property(PropertyInfo &property) const {
 
 		property.hint_string = hint_string;
 	}
+
+	if (property.name == "rect_scale") {
+		property.hint = PROPERTY_HINT_LINK;
+	}
 }
 
 Control *Control::get_parent_control() const {