ztc0611/godot

[macOS] Add default Window and Help menus, allow special menu customization.

platform/macos/display_server_macos.h
@@ -139,6 +139,8 @@ private:
 	String rendering_driver;
 
 	NSMenu *apple_menu = nullptr;
+	NSMenu *window_menu = nullptr;
+	NSMenu *help_menu = nullptr;
 	NSMenu *dock_menu = nullptr;
 	struct MenuData {
 		Callable open;
@@ -226,7 +228,8 @@ private:
 
 	static NSCursor *_cursor_from_selector(SEL p_selector, SEL p_fallback = nil);
 
-	bool _has_help_menu() const;
+	int _get_special_menu_start(const NSMenu *p_menu) const;
+	int _get_special_menu_count(const NSMenu *p_menu) const;
 	NSMenuItem *_menu_add_item(const String &p_menu_root, const String &p_label, Key p_accel, int p_index, int *r_out);
 
 public:
@@ -319,6 +322,8 @@ public:
 
 	virtual void global_menu_remove_item(const String &p_menu_root, int p_idx) override;
 	virtual void global_menu_clear(const String &p_menu_root) override;
+
+	virtual Dictionary global_menu_get_special_menu_roots() const override;
 
 	virtual bool tts_is_speaking() const override;
 	virtual bool tts_is_paused() const override;

scene/gui/menu_bar.cpp
@@ -249,11 +249,13 @@ String MenuBar::bind_global_menu() {
 	Vector<PopupMenu *> popups = _get_popups();
 	for (int i = 0; i < menu_cache.size(); i++) {
 		String submenu_name = popups[i]->bind_global_menu();
-		int index = ds->global_menu_add_submenu_item("_main", menu_cache[i].name, submenu_name, global_start_idx + i);
-		ds->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(i));
-		ds->global_menu_set_item_hidden("_main", index, menu_cache[i].hidden);
-		ds->global_menu_set_item_disabled("_main", index, menu_cache[i].disabled);
-		ds->global_menu_set_item_tooltip("_main", index, menu_cache[i].tooltip);
+		if (!popups[i]->is_special_menu()) {
+			int index = ds->global_menu_add_submenu_item("_main", menu_cache[i].name, submenu_name, global_start_idx + i);
+			ds->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(i));
+			ds->global_menu_set_item_hidden("_main", index, menu_cache[i].hidden);
+			ds->global_menu_set_item_disabled("_main", index, menu_cache[i].disabled);
+			ds->global_menu_set_item_tooltip("_main", index, menu_cache[i].tooltip);
+		}
 	}
 
 	return global_menu_name;
@@ -268,8 +270,10 @@ void MenuBar::unbind_global_menu() {
 	int global_start = _find_global_start_index();
 	Vector<PopupMenu *> popups = _get_popups();
 	for (int i = menu_cache.size() - 1; i >= 0; i--) {
-		popups[i]->unbind_global_menu();
-		ds->global_menu_remove_item("_main", global_start + i);
+		if (!popups[i]->is_special_menu()) {
+			popups[i]->unbind_global_menu();
+			ds->global_menu_remove_item("_main", global_start + i);
+		}
 	}
 
 	global_menu_name = String();
@@ -558,8 +562,10 @@ void MenuBar::add_child_notify(Node *p_child) {
 
 	if (!global_menu_name.is_empty()) {
 		String submenu_name = pm->bind_global_menu();
-		int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, _find_global_start_index() + menu_cache.size() - 1);
-		DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(menu_cache.size() - 1));
+		if (!pm->is_special_menu()) {
+			int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, _find_global_start_index() + menu_cache.size() - 1);
+			DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(menu_cache.size() - 1));
+		}
 	}
 	update_minimum_size();
 }
@@ -587,14 +593,16 @@ void MenuBar::move_child_notify(Node *p_child) {
 	menu_cache.insert(new_idx, menu);
 
 	if (!global_menu_name.is_empty()) {
-		int global_start = _find_global_start_index();
-		if (old_idx != -1) {
-			DisplayServer::get_singleton()->global_menu_remove_item("_main", global_start + old_idx);
-		}
-		if (new_idx != -1) {
-			String submenu_name = pm->bind_global_menu();
-			int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, global_start + new_idx);
-			DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(new_idx));
+		if (!pm->is_special_menu()) {
+			int global_start = _find_global_start_index();
+			if (old_idx != -1) {
+				DisplayServer::get_singleton()->global_menu_remove_item("_main", global_start + old_idx);
+			}
+			if (new_idx != -1) {
+				String submenu_name = pm->bind_global_menu();
+				int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, global_start + new_idx);
+				DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(new_idx));
+			}
 		}
 	}
 }
@@ -612,8 +620,10 @@ void MenuBar::remove_child_notify(Node *p_child) {
 	menu_cache.remove_at(idx);
 
 	if (!global_menu_name.is_empty()) {
-		pm->unbind_global_menu();
-		DisplayServer::get_singleton()->global_menu_remove_item("_main", _find_global_start_index() + idx);
+		if (!pm->is_special_menu()) {
+			pm->unbind_global_menu();
+			DisplayServer::get_singleton()->global_menu_remove_item("_main", _find_global_start_index() + idx);
+		}
 	}
 
 	p_child->remove_meta("_menu_name");

servers/display_server.cpp
@@ -267,6 +267,11 @@ void DisplayServer::global_menu_clear(const String &p_menu_root) {
 	WARN_PRINT("Global menus not supported by this display server.");
 }
 
+Dictionary DisplayServer::global_menu_get_special_menu_roots() const {
+	WARN_PRINT("Global menus not supported by this display server.");
+	return Dictionary();
+}
+
 bool DisplayServer::tts_is_speaking() const {
 	WARN_PRINT("TTS is not supported by this display server.");
 	return false;
@@ -651,6 +656,8 @@ void DisplayServer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("global_menu_remove_item", "menu_root", "idx"), &DisplayServer::global_menu_remove_item);
 	ClassDB::bind_method(D_METHOD("global_menu_clear", "menu_root"), &DisplayServer::global_menu_clear);
+
+	ClassDB::bind_method(D_METHOD("global_menu_get_special_menu_roots"), &DisplayServer::global_menu_get_special_menu_roots);
 
 	ClassDB::bind_method(D_METHOD("tts_is_speaking"), &DisplayServer::tts_is_speaking);
 	ClassDB::bind_method(D_METHOD("tts_is_paused"), &DisplayServer::tts_is_paused);

servers/display_server.h
@@ -185,6 +185,8 @@ public:
 	virtual void global_menu_remove_item(const String &p_menu_root, int p_idx);
 	virtual void global_menu_clear(const String &p_menu_root);
 
+	virtual Dictionary global_menu_get_special_menu_roots() const;
+
 	struct TTSUtterance {
 		String text;
 		String voice;

rivie13/Phoenix-Agentic-Engine

platform/macos/display_server_macos.h
@@ -140,6 +140,8 @@ private:
 	String rendering_driver;
 
 	NSMenu *apple_menu = nullptr;
+	NSMenu *window_menu = nullptr;
+	NSMenu *help_menu = nullptr;
 	NSMenu *dock_menu = nullptr;
 	struct MenuData {
 		Callable open;
@@ -226,7 +228,8 @@ private:
 
 	static NSCursor *_cursor_from_selector(SEL p_selector, SEL p_fallback = nil);
 
-	bool _has_help_menu() const;
+	int _get_system_menu_start(const NSMenu *p_menu) const;
+	int _get_system_menu_count(const NSMenu *p_menu) const;
 	NSMenuItem *_menu_add_item(const String &p_menu_root, const String &p_label, Key p_accel, int p_index, int *r_out);
 
 public:
@@ -320,6 +323,8 @@ public:
 
 	virtual void global_menu_remove_item(const String &p_menu_root, int p_idx) override;
 	virtual void global_menu_clear(const String &p_menu_root) override;
+
+	virtual Dictionary global_menu_get_system_menu_roots() const override;
 
 	virtual bool tts_is_speaking() const override;
 	virtual bool tts_is_paused() const override;

scene/gui/menu_bar.cpp
@@ -249,11 +249,13 @@ String MenuBar::bind_global_menu() {
 	Vector<PopupMenu *> popups = _get_popups();
 	for (int i = 0; i < menu_cache.size(); i++) {
 		String submenu_name = popups[i]->bind_global_menu();
-		int index = ds->global_menu_add_submenu_item("_main", menu_cache[i].name, submenu_name, global_start_idx + i);
-		ds->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(i));
-		ds->global_menu_set_item_hidden("_main", index, menu_cache[i].hidden);
-		ds->global_menu_set_item_disabled("_main", index, menu_cache[i].disabled);
-		ds->global_menu_set_item_tooltip("_main", index, menu_cache[i].tooltip);
+		if (!popups[i]->is_system_menu()) {
+			int index = ds->global_menu_add_submenu_item("_main", menu_cache[i].name, submenu_name, global_start_idx + i);
+			ds->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(i));
+			ds->global_menu_set_item_hidden("_main", index, menu_cache[i].hidden);
+			ds->global_menu_set_item_disabled("_main", index, menu_cache[i].disabled);
+			ds->global_menu_set_item_tooltip("_main", index, menu_cache[i].tooltip);
+		}
 	}
 
 	return global_menu_name;
@@ -268,8 +270,10 @@ void MenuBar::unbind_global_menu() {
 	int global_start = _find_global_start_index();
 	Vector<PopupMenu *> popups = _get_popups();
 	for (int i = menu_cache.size() - 1; i >= 0; i--) {
-		popups[i]->unbind_global_menu();
-		ds->global_menu_remove_item("_main", global_start + i);
+		if (!popups[i]->is_system_menu()) {
+			popups[i]->unbind_global_menu();
+			ds->global_menu_remove_item("_main", global_start + i);
+		}
 	}
 
 	global_menu_name = String();
@@ -558,8 +562,10 @@ void MenuBar::add_child_notify(Node *p_child) {
 
 	if (!global_menu_name.is_empty()) {
 		String submenu_name = pm->bind_global_menu();
-		int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, _find_global_start_index() + menu_cache.size() - 1);
-		DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(menu_cache.size() - 1));
+		if (!pm->is_system_menu()) {
+			int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, _find_global_start_index() + menu_cache.size() - 1);
+			DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(menu_cache.size() - 1));
+		}
 	}
 	update_minimum_size();
 }
@@ -587,14 +593,16 @@ void MenuBar::move_child_notify(Node *p_child) {
 	menu_cache.insert(new_idx, menu);
 
 	if (!global_menu_name.is_empty()) {
-		int global_start = _find_global_start_index();
-		if (old_idx != -1) {
-			DisplayServer::get_singleton()->global_menu_remove_item("_main", global_start + old_idx);
-		}
-		if (new_idx != -1) {
-			String submenu_name = pm->bind_global_menu();
-			int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, global_start + new_idx);
-			DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(new_idx));
+		if (!pm->is_system_menu()) {
+			int global_start = _find_global_start_index();
+			if (old_idx != -1) {
+				DisplayServer::get_singleton()->global_menu_remove_item("_main", global_start + old_idx);
+			}
+			if (new_idx != -1) {
+				String submenu_name = pm->bind_global_menu();
+				int index = DisplayServer::get_singleton()->global_menu_add_submenu_item("_main", atr(menu.name), submenu_name, global_start + new_idx);
+				DisplayServer::get_singleton()->global_menu_set_item_tag("_main", index, global_menu_name + "#" + itos(new_idx));
+			}
 		}
 	}
 }
@@ -612,8 +620,10 @@ void MenuBar::remove_child_notify(Node *p_child) {
 	menu_cache.remove_at(idx);
 
 	if (!global_menu_name.is_empty()) {
-		pm->unbind_global_menu();
-		DisplayServer::get_singleton()->global_menu_remove_item("_main", _find_global_start_index() + idx);
+		if (!pm->is_system_menu()) {
+			pm->unbind_global_menu();
+			DisplayServer::get_singleton()->global_menu_remove_item("_main", _find_global_start_index() + idx);
+		}
 	}
 
 	p_child->remove_meta("_menu_name");

servers/display_server.cpp
@@ -267,6 +267,11 @@ void DisplayServer::global_menu_clear(const String &p_menu_root) {
 	WARN_PRINT("Global menus not supported by this display server.");
 }
 
+Dictionary DisplayServer::global_menu_get_system_menu_roots() const {
+	WARN_PRINT("Global menus not supported by this display server.");
+	return Dictionary();
+}
+
 bool DisplayServer::tts_is_speaking() const {
 	WARN_PRINT("TTS is not supported by this display server.");
 	return false;
@@ -651,6 +656,8 @@ void DisplayServer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("global_menu_remove_item", "menu_root", "idx"), &DisplayServer::global_menu_remove_item);
 	ClassDB::bind_method(D_METHOD("global_menu_clear", "menu_root"), &DisplayServer::global_menu_clear);
+
+	ClassDB::bind_method(D_METHOD("global_menu_get_system_menu_roots"), &DisplayServer::global_menu_get_system_menu_roots);
 
 	ClassDB::bind_method(D_METHOD("tts_is_speaking"), &DisplayServer::tts_is_speaking);
 	ClassDB::bind_method(D_METHOD("tts_is_paused"), &DisplayServer::tts_is_paused);

servers/display_server.h
@@ -185,6 +185,8 @@ public:
 	virtual void global_menu_remove_item(const String &p_menu_root, int p_idx);
 	virtual void global_menu_clear(const String &p_menu_root);
 
+	virtual Dictionary global_menu_get_system_menu_roots() const;
+
 	struct TTSUtterance {
 		String text;
 		String voice;