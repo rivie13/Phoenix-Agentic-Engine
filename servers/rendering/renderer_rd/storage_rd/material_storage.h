sheddy1/Synk

Optimize RenderForwardClustered::_setup_render_pass_uniform_set by reducing Vector allocations during push_back operations

servers/rendering/renderer_rd/storage_rd/material_storage.h
@@ -115,7 +115,8 @@ public:
 			return rids[p_filter][p_repeat];
 		}
 
-		Vector<RD::Uniform> get_uniforms(int p_first_index) const;
+		template <typename Collection>
+		void append_uniforms(Collection &uniforms, int p_first_index) const;
 		bool is_valid() const;
 		bool is_null() const;
 	};

rivie13/Phoenix-Agentic-Engine

servers/rendering/renderer_rd/storage_rd/material_storage.h
@@ -128,7 +128,8 @@ public:
 			return rids[p_filter][p_repeat];
 		}
 
-		Vector<RD::Uniform> get_uniforms(int p_first_index) const;
+		template <typename Collection>
+		void append_uniforms(Collection &p_uniforms, int p_first_index) const;
 		bool is_valid() const;
 		bool is_null() const;
 	};