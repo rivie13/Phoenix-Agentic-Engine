okamstudio/godot

Editor: Improve capsule gizmos

editor/plugins/gizmos/gizmo_3d_helper.cpp
@@ -156,7 +156,10 @@ String Gizmo3DHelper::cylinder_get_handle_name(int p_id) const {
 	}
 }
 
-void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position) {
+void Gizmo3DHelper::cylinder_or_capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position, bool p_is_capsule) {
+	real_t initial_radius = initial_value.operator Vector2().x;
+	real_t initial_height = initial_value.operator Vector2().y;
+
 	int sign = p_id == 2 ? -1 : 1;
 	int axis = p_id == 0 ? 0 : 1;
 
@@ -178,9 +181,13 @@ void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, re
 		}
 		r_radius = d;
 		r_cylinder_position = initial_transform.get_origin();
-	} else if (p_id == 1 || p_id == 2) {
-		real_t initial_height = initial_value;
 
+		if (p_is_capsule) {
+			r_height = MAX(initial_height, r_radius * 2.0);
+		} else {
+			r_height = initial_height;
+		}
+	} else if (p_id == 1 || p_id == 2) {
 		// Adjust height.
 		if (Input::get_singleton()->is_key_pressed(Key::ALT)) {
 			r_height = d * 2.0;
@@ -200,6 +207,12 @@ void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, re
 			offset[axis] = (r_height - initial_height) * 0.5 * sign;
 			r_cylinder_position = initial_transform.xform(offset);
 		}
+
+		if (p_is_capsule) {
+			r_radius = MIN(initial_radius, r_height / 2.0);
+		} else {
+			r_radius = initial_radius;
+		}
 	}
 }
 
@@ -212,25 +225,19 @@ void Gizmo3DHelper::cylinder_commit_handle(int p_id, const String &p_radius_acti
 	}
 
 	if (p_cancel) {
-		if (p_id == 0) {
-			p_radius_object->set(p_radius_property, initial_value);
-		} else {
-			p_height_object->set(p_height_property, initial_value);
-		}
+		p_radius_object->set(p_radius_property, initial_value.operator Vector2().x);
+		p_height_object->set(p_height_property, initial_value.operator Vector2().y);
 		p_position_object->set(p_position_property, initial_transform.get_origin());
 		return;
 	}
 
 	EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
 	ur->create_action(p_id == 0 ? p_radius_action_name : p_height_action_name);
-	if (p_id == 0) {
-		ur->add_do_property(p_radius_object, p_radius_property, p_radius_object->get(p_radius_property));
-		ur->add_undo_property(p_radius_object, p_radius_property, initial_value);
-	} else {
-		ur->add_do_property(p_height_object, p_height_property, p_height_object->get(p_height_property));
-		ur->add_do_property(p_position_object, p_position_property, p_position_object->get(p_position_property));
-		ur->add_undo_property(p_height_object, p_height_property, initial_value);
-		ur->add_undo_property(p_position_object, p_position_property, initial_transform.get_origin());
-	}
+	ur->add_do_property(p_radius_object, p_radius_property, p_radius_object->get(p_radius_property));
+	ur->add_undo_property(p_radius_object, p_radius_property, initial_value.operator Vector2().x);
+	ur->add_do_property(p_height_object, p_height_property, p_height_object->get(p_height_property));
+	ur->add_undo_property(p_height_object, p_height_property, initial_value.operator Vector2().y);
+	ur->add_do_property(p_position_object, p_position_property, p_position_object->get(p_position_property));
+	ur->add_undo_property(p_position_object, p_position_property, initial_transform.get_origin());
 	ur->commit_action();
 }

editor/plugins/gizmos/gizmo_3d_helper.h
@@ -42,19 +42,46 @@ class Gizmo3DHelper : public RefCounted {
 	Variant initial_value;
 	Transform3D initial_transform;
 
+private:
+	void cylinder_or_capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position, bool p_is_capsule);
+
 public:
+	/**
+	 * Initializes a new action involving a handle.
+	 *
+	 * Depending on the type of gizmo that will be used, different formats for the `p_initial_value` are required:
+	 * Box: The size of the box as `Vector3`
+	 * Cylinder or Capsule: A `Vector2` of the form `Vector2(radius, height)`
+	 */
 	void initialize_handle_action(const Variant &p_initial_value, const Transform3D &p_initial_transform);
 	void get_segment(Camera3D *p_camera, const Point2 &p_point, Vector3 *r_segment);
+
+	// Box
 
 	Vector<Vector3> box_get_handles(const Vector3 &p_box_size);
 	String box_get_handle_name(int p_id) const;
 	void box_set_handle(const Vector3 p_segment[2], int p_id, Vector3 &r_box_size, Vector3 &r_box_position);
 	void box_commit_handle(const String &p_action_name, bool p_cancel, Object *p_position_object, Object *p_size_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_size_property = "size");
 
+	// Cylinder
+
 	Vector<Vector3> cylinder_get_handles(real_t p_height, real_t p_radius);
 	String cylinder_get_handle_name(int p_id) const;
-	void cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position);
+	_FORCE_INLINE_ void cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position) {
+		cylinder_or_capsule_set_handle(p_segment, p_id, r_height, r_radius, r_cylinder_position, false);
+	}
 	void cylinder_commit_handle(int p_id, const String &p_radius_action_name, const String &p_height_action_name, bool p_cancel, Object *p_position_object, Object *p_height_object = nullptr, Object *p_radius_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_height_property = "height", const StringName &p_radius_property = "radius");
+
+	// Capsule
+
+	_FORCE_INLINE_ Vector<Vector3> capsule_get_handles(real_t p_height, real_t p_radius) { return cylinder_get_handles(p_height, p_radius); }
+	_FORCE_INLINE_ String capsule_get_handle_name(int p_id) { return cylinder_get_handle_name(p_id); }
+	_FORCE_INLINE_ void capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_capsule_position) {
+		cylinder_or_capsule_set_handle(p_segment, p_id, r_height, r_radius, r_capsule_position, true);
+	}
+	_FORCE_INLINE_ void capsule_commit_handle(int p_id, const String &p_radius_action_name, const String &p_height_action_name, bool p_cancel, Object *p_position_object, Object *p_height_object = nullptr, Object *p_radius_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_height_property = "height", const StringName &p_radius_property = "radius") {
+		cylinder_commit_handle(p_id, p_radius_action_name, p_height_action_name, p_cancel, p_position_object, p_height_object, p_radius_object, p_position_property, p_height_property, p_radius_property);
+	}
 };
 
 #endif // GIZMO_3D_HELPER_H

rivie13/Phoenix-Agentic-Engine

editor/plugins/gizmos/gizmo_3d_helper.cpp
@@ -156,7 +156,10 @@ String Gizmo3DHelper::cylinder_get_handle_name(int p_id) const {
 	}
 }
 
-void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position) {
+void Gizmo3DHelper::_cylinder_or_capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position, bool p_is_capsule) {
+	real_t initial_radius = initial_value.operator Vector2().x;
+	real_t initial_height = initial_value.operator Vector2().y;
+
 	int sign = p_id == 2 ? -1 : 1;
 	int axis = p_id == 0 ? 0 : 1;
 
@@ -178,9 +181,13 @@ void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, re
 		}
 		r_radius = d;
 		r_cylinder_position = initial_transform.get_origin();
-	} else if (p_id == 1 || p_id == 2) {
-		real_t initial_height = initial_value;
 
+		if (p_is_capsule) {
+			r_height = MAX(initial_height, r_radius * 2.0);
+		} else {
+			r_height = initial_height;
+		}
+	} else if (p_id == 1 || p_id == 2) {
 		// Adjust height.
 		if (Input::get_singleton()->is_key_pressed(Key::ALT)) {
 			r_height = d * 2.0;
@@ -200,6 +207,12 @@ void Gizmo3DHelper::cylinder_set_handle(const Vector3 p_segment[2], int p_id, re
 			offset[axis] = (r_height - initial_height) * 0.5 * sign;
 			r_cylinder_position = initial_transform.xform(offset);
 		}
+
+		if (p_is_capsule) {
+			r_radius = MIN(initial_radius, r_height / 2.0);
+		} else {
+			r_radius = initial_radius;
+		}
 	}
 }
 
@@ -212,25 +225,19 @@ void Gizmo3DHelper::cylinder_commit_handle(int p_id, const String &p_radius_acti
 	}
 
 	if (p_cancel) {
-		if (p_id == 0) {
-			p_radius_object->set(p_radius_property, initial_value);
-		} else {
-			p_height_object->set(p_height_property, initial_value);
-		}
+		p_radius_object->set(p_radius_property, initial_value.operator Vector2().x);
+		p_height_object->set(p_height_property, initial_value.operator Vector2().y);
 		p_position_object->set(p_position_property, initial_transform.get_origin());
 		return;
 	}
 
 	EditorUndoRedoManager *ur = EditorUndoRedoManager::get_singleton();
 	ur->create_action(p_id == 0 ? p_radius_action_name : p_height_action_name);
-	if (p_id == 0) {
-		ur->add_do_property(p_radius_object, p_radius_property, p_radius_object->get(p_radius_property));
-		ur->add_undo_property(p_radius_object, p_radius_property, initial_value);
-	} else {
-		ur->add_do_property(p_height_object, p_height_property, p_height_object->get(p_height_property));
-		ur->add_do_property(p_position_object, p_position_property, p_position_object->get(p_position_property));
-		ur->add_undo_property(p_height_object, p_height_property, initial_value);
-		ur->add_undo_property(p_position_object, p_position_property, initial_transform.get_origin());
-	}
+	ur->add_do_property(p_radius_object, p_radius_property, p_radius_object->get(p_radius_property));
+	ur->add_undo_property(p_radius_object, p_radius_property, initial_value.operator Vector2().x);
+	ur->add_do_property(p_height_object, p_height_property, p_height_object->get(p_height_property));
+	ur->add_undo_property(p_height_object, p_height_property, initial_value.operator Vector2().y);
+	ur->add_do_property(p_position_object, p_position_property, p_position_object->get(p_position_property));
+	ur->add_undo_property(p_position_object, p_position_property, initial_transform.get_origin());
 	ur->commit_action();
 }

editor/plugins/gizmos/gizmo_3d_helper.h
@@ -41,17 +41,44 @@ class Gizmo3DHelper : public RefCounted {
 	Variant initial_value;
 	Transform3D initial_transform;
 
+private:
+	void _cylinder_or_capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position, bool p_is_capsule);
+
 public:
+	/**
+	 * Initializes a new action involving a handle.
+	 *
+	 * Depending on the type of gizmo that will be used, different formats for the `p_initial_value` are required:
+	 * Box: The size of the box as `Vector3`
+	 * Cylinder or Capsule: A `Vector2` of the form `Vector2(radius, height)`
+	 */
 	void initialize_handle_action(const Variant &p_initial_value, const Transform3D &p_initial_transform);
 	void get_segment(Camera3D *p_camera, const Point2 &p_point, Vector3 *r_segment);
+
+	// Box
 
 	Vector<Vector3> box_get_handles(const Vector3 &p_box_size);
 	String box_get_handle_name(int p_id) const;
 	void box_set_handle(const Vector3 p_segment[2], int p_id, Vector3 &r_box_size, Vector3 &r_box_position);
 	void box_commit_handle(const String &p_action_name, bool p_cancel, Object *p_position_object, Object *p_size_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_size_property = "size");
 
+	// Cylinder
+
 	Vector<Vector3> cylinder_get_handles(real_t p_height, real_t p_radius);
 	String cylinder_get_handle_name(int p_id) const;
-	void cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position);
+	_FORCE_INLINE_ void cylinder_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_cylinder_position) {
+		_cylinder_or_capsule_set_handle(p_segment, p_id, r_height, r_radius, r_cylinder_position, false);
+	}
 	void cylinder_commit_handle(int p_id, const String &p_radius_action_name, const String &p_height_action_name, bool p_cancel, Object *p_position_object, Object *p_height_object = nullptr, Object *p_radius_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_height_property = "height", const StringName &p_radius_property = "radius");
+
+	// Capsule
+
+	_FORCE_INLINE_ Vector<Vector3> capsule_get_handles(real_t p_height, real_t p_radius) { return cylinder_get_handles(p_height, p_radius); }
+	_FORCE_INLINE_ String capsule_get_handle_name(int p_id) { return cylinder_get_handle_name(p_id); }
+	_FORCE_INLINE_ void capsule_set_handle(const Vector3 p_segment[2], int p_id, real_t &r_height, real_t &r_radius, Vector3 &r_capsule_position) {
+		_cylinder_or_capsule_set_handle(p_segment, p_id, r_height, r_radius, r_capsule_position, true);
+	}
+	_FORCE_INLINE_ void capsule_commit_handle(int p_id, const String &p_radius_action_name, const String &p_height_action_name, bool p_cancel, Object *p_position_object, Object *p_height_object = nullptr, Object *p_radius_object = nullptr, const StringName &p_position_property = "global_position", const StringName &p_height_property = "height", const StringName &p_radius_property = "radius") {
+		cylinder_commit_handle(p_id, p_radius_action_name, p_height_action_name, p_cancel, p_position_object, p_height_object, p_radius_object, p_position_property, p_height_property, p_radius_property);
+	}
 };