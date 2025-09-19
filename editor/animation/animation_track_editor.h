godotengine/godot

Add button to add animation player in track editor

editor/animation_track_editor.h
@@ -594,7 +594,10 @@ class AnimationTrackEditor : public VBoxContainer {
 	AnimationBezierTrackEdit *bezier_edit = nullptr;
 	VBoxContainer *timeline_vbox = nullptr;
 
+	VBoxContainer *info_message_vbox = nullptr;
 	Label *info_message = nullptr;
+	Button *add_animation_player = nullptr;
+	void _add_animation_player();
 
 	AnimationTimelineEdit *timeline = nullptr;
 	AnimationMarkerEdit *marker_edit = nullptr;
@@ -810,6 +813,8 @@ class AnimationTrackEditor : public VBoxContainer {
 	void _auto_fit();
 	void _auto_fit_bezier();
 
+	void _root_node_changed(Node *node, bool removed);
+	void _scene_changed();
 	void _selection_changed();
 
 	ConfirmationDialog *track_copy_dialog = nullptr;

rivie13/Phoenix-Agentic-Engine

editor/animation/animation_track_editor.h
@@ -603,7 +603,10 @@ class AnimationTrackEditor : public VBoxContainer {
 	AnimationBezierTrackEdit *bezier_edit = nullptr;
 	VBoxContainer *timeline_vbox = nullptr;
 
+	VBoxContainer *info_message_vbox = nullptr;
 	Label *info_message = nullptr;
+	Button *add_animation_player = nullptr;
+	void _add_animation_player();
 
 	AnimationTimelineEdit *timeline = nullptr;
 	AnimationMarkerEdit *marker_edit = nullptr;
@@ -826,6 +829,8 @@ class AnimationTrackEditor : public VBoxContainer {
 	void _auto_fit();
 	void _auto_fit_bezier();
 
+	void _root_node_changed(Node *p_node, bool p_removed);
+	void _scene_changed();
 	void _selection_changed();
 
 	ConfirmationDialog *track_copy_dialog = nullptr;