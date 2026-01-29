godotengine/godot

Replace DockSlot with DockTabContainer

editor/docks/editor_dock_manager.h
@@ -73,6 +73,7 @@ public:
 
 class DockContextPopup;
 class EditorDockDragHint;
+class DockTabContainer;
 
 class EditorDockManager : public Object {
 	GDCLASS(EditorDockManager, Object);
@@ -88,15 +89,12 @@ private:
 	Vector<DockSplitContainer *> vsplits;
 	Vector<DockSplitContainer *> hsplits;
 
-	struct DockSlot {
-		TabContainer *container = nullptr;
-		EditorDockDragHint *drag_hint = nullptr;
-		DockConstants::DockLayout layout = DockConstants::DOCK_LAYOUT_VERTICAL;
-	};
-
-	DockSlot dock_slots[DockConstants::DOCK_SLOT_MAX];
+	DockTabContainer *dock_slots[DockConstants::DOCK_SLOT_MAX];
 	Vector<WindowWrapper *> dock_windows;
 	LocalVector<EditorDock *> all_docks;
+
+	HashSet<EditorDock *> dirty_docks;
+	bool update_queued = false;
 
 	EditorDock *dock_tab_dragged = nullptr;
 	bool docks_visible = true;
@@ -109,8 +107,6 @@ private:
 	EditorDock *_get_dock_tab_dragged();
 	void _dock_drag_stopped();
 	void _dock_split_dragged(int p_offset);
-	void _dock_container_popup(int p_tab_idx, TabContainer *p_dock_container);
-	void _dock_container_update_visibility(TabContainer *p_dock_container);
 	void _update_layout();
 
 	void _docks_menu_option(int p_id);
@@ -120,10 +116,10 @@ private:
 	void _open_dock_in_window(EditorDock *p_dock, bool p_show_window = true, bool p_reset_size = false);
 	void _restore_dock_to_saved_window(EditorDock *p_dock, const Dictionary &p_window_dump);
 
-	void _move_dock_tab_index(EditorDock *p_dock, int p_tab_index, bool p_set_current);
 	void _move_dock(EditorDock *p_dock, Control *p_target, int p_tab_index = -1, bool p_set_current = true);
 
-	void _update_tab_style(EditorDock *p_dock);
+	void _queue_update_tab_style(EditorDock *p_dock);
+	void _update_dirty_dock_tabs();
 
 public:
 	static EditorDockManager *get_singleton() { return singleton; }
@@ -134,7 +130,7 @@ public:
 
 	void add_vsplit(DockSplitContainer *p_split);
 	void add_hsplit(DockSplitContainer *p_split);
-	void register_dock_slot(DockConstants::DockSlot p_dock_slot, TabContainer *p_tab_container, DockConstants::DockLayout p_layout);
+	void register_dock_slot(DockTabContainer *p_tab_container);
 	int get_hsplit_count() const;
 	int get_vsplit_count() const;
 	PopupMenu *get_docks_menu();
@@ -147,8 +143,6 @@ public:
 	void open_dock(EditorDock *p_dock, bool p_set_current = true);
 	void focus_dock(EditorDock *p_dock);
 
-	TabContainer *get_dock_tab_container(Control *p_dock) const;
-
 	void set_docks_visible(bool p_show);
 	bool are_docks_visible() const;
 
@@ -156,36 +150,6 @@ public:
 	void remove_dock(EditorDock *p_dock);
 
 	EditorDockManager();
-};
-
-class EditorDockDragHint : public Control {
-	GDCLASS(EditorDockDragHint, Control);
-
-private:
-	EditorDockManager *dock_manager = nullptr;
-	DockConstants::DockSlot occupied_slot = DockConstants::DOCK_SLOT_MAX;
-	TabBar *drop_tabbar = nullptr;
-
-	Color valid_drop_color;
-	Ref<StyleBoxFlat> dock_drop_highlight;
-	bool can_drop_dock = false;
-	bool mouse_inside = false;
-	bool mouse_inside_tabbar = false;
-
-	void _drag_move_tab(int p_from_index, int p_to_index);
-	void _drag_move_tab_from(TabBar *p_from_tabbar, int p_from_index, int p_to_index);
-
-protected:
-	virtual void gui_input(const Ref<InputEvent> &p_event) override;
-
-	void _notification(int p_what);
-	bool can_drop_data(const Point2 &p_point, const Variant &p_data) const override;
-	void drop_data(const Point2 &p_point, const Variant &p_data) override;
-
-public:
-	void set_slot(DockConstants::DockSlot p_slot);
-
-	EditorDockDragHint();
 };
 
 class DockContextPopup : public PopupPanel {
@@ -211,7 +175,6 @@ private:
 	void _tab_move_right();
 	void _close_dock();
 	void _float_dock();
-	bool _is_slot_available(int p_slot) const;
 
 	void _dock_select_input(const Ref<InputEvent> &p_input);
 	void _dock_select_mouse_exited();
@@ -223,9 +186,7 @@ protected:
 	void _notification(int p_what);
 
 public:
-	void select_current_dock_in_dock_slot(int p_dock_slot);
 	void set_dock(EditorDock *p_dock);
-	EditorDock *get_dock() const;
 	void docks_updated();
 
 	DockContextPopup();

editor/editor_node.cpp
@@ -8244,13 +8244,19 @@ EditorNode::EditorNode() {
 	left_l_vsplit->set_vertical(true);
 	left_l_hsplit->add_child(left_l_vsplit);
 
-	TabContainer *dock_slot[DockConstants::DOCK_SLOT_MAX];
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_UL] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_UL]->set_name("DockSlotLeftUL");
-	left_l_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_LEFT_UL]);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_BL] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_BL]->set_name("DockSlotLeftBL");
-	left_l_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_LEFT_BL]);
+	DockTabContainer *dock_slots[DockConstants::DOCK_SLOT_MAX];
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_LEFT_UL));
+		dock_container->set_name("DockSlotLeftUL");
+		left_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_LEFT_BL));
+		dock_container->set_name("DockSlotLeftBL");
+		left_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	left_r_hsplit = memnew(DockSplitContainer);
 	left_r_hsplit->set_name("DockHSplitLeftR");
@@ -8259,12 +8265,18 @@ EditorNode::EditorNode() {
 	left_r_vsplit->set_name("DockVSplitLeftR");
 	left_r_vsplit->set_vertical(true);
 	left_r_hsplit->add_child(left_r_vsplit);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_UR] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_UR]->set_name("DockSlotLeftUR");
-	left_r_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_LEFT_UR]);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_BR] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_LEFT_BR]->set_name("DockSlotLeftBR");
-	left_r_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_LEFT_BR]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_LEFT_UR));
+		dock_container->set_name("DockSlotLeftUR");
+		left_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_LEFT_BR));
+		dock_container->set_name("DockSlotLeftBR");
+		left_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	main_hsplit = memnew(DockSplitContainer);
 	main_hsplit->set_name("DockHSplitMain");
@@ -8290,23 +8302,35 @@ EditorNode::EditorNode() {
 	right_l_vsplit->set_name("DockVSplitRightL");
 	right_l_vsplit->set_vertical(true);
 	right_hsplit->add_child(right_l_vsplit);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_UL] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_UL]->set_name("DockSlotRightUL");
-	right_l_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_RIGHT_UL]);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_BL] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_BL]->set_name("DockSlotRightBL");
-	right_l_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_RIGHT_BL]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_RIGHT_UL));
+		dock_container->set_name("DockSlotRightUL");
+		right_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_RIGHT_BL));
+		dock_container->set_name("DockSlotRightBL");
+		right_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	right_r_vsplit = memnew(DockSplitContainer);
 	right_r_vsplit->set_name("DockVSplitRightR");
 	right_r_vsplit->set_vertical(true);
 	right_hsplit->add_child(right_r_vsplit);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_UR] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_UR]->set_name("DockSlotRightUR");
-	right_r_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_RIGHT_UR]);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_BR] = memnew(TabContainer);
-	dock_slot[DockConstants::DOCK_SLOT_RIGHT_BR]->set_name("DockSlotRightBR");
-	right_r_vsplit->add_child(dock_slot[DockConstants::DOCK_SLOT_RIGHT_BR]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_RIGHT_UR));
+		dock_container->set_name("DockSlotRightUR");
+		right_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(DockConstants::DOCK_SLOT_RIGHT_BR));
+		dock_container->set_name("DockSlotRightBR");
+		right_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	editor_dock_manager = memnew(EditorDockManager);
 
@@ -8322,7 +8346,7 @@ EditorNode::EditorNode() {
 	editor_dock_manager->add_hsplit(right_hsplit);
 
 	for (int i = 0; i < DockConstants::DOCK_SLOT_BOTTOM; i++) {
-		editor_dock_manager->register_dock_slot((DockConstants::DockSlot)i, dock_slot[i], DockConstants::DOCK_LAYOUT_VERTICAL);
+		editor_dock_manager->register_dock_slot(dock_slots[i]);
 	}
 
 	editor_layout_save_delay_timer = memnew(Timer);
@@ -8790,8 +8814,7 @@ EditorNode::EditorNode() {
 	// Bottom panels.
 
 	bottom_panel = memnew(EditorBottomPanel);
-	editor_dock_manager->register_dock_slot(DockConstants::DOCK_SLOT_BOTTOM, bottom_panel, DockConstants::DOCK_LAYOUT_HORIZONTAL);
-	bottom_panel->set_theme_type_variation("BottomPanel");
+	editor_dock_manager->register_dock_slot(bottom_panel);
 	center_split->add_child(bottom_panel);
 	center_split->set_dragger_visibility(SplitContainer::DRAGGER_HIDDEN);
 

editor/gui/editor_bottom_panel.cpp
@@ -39,6 +39,7 @@
 #include "editor/gui/editor_version_button.h"
 #include "editor/scene/editor_scene_tabs.h"
 #include "editor/settings/editor_command_palette.h"
+#include "editor/settings/editor_settings.h"
 #include "editor/themes/editor_scale.h"
 #include "scene/gui/box_container.h"
 #include "scene/gui/button.h"
@@ -47,10 +48,6 @@
 
 void EditorBottomPanel::_notification(int p_what) {
 	switch (p_what) {
-		case NOTIFICATION_READY: {
-			layout_popup = get_popup();
-		} break;
-
 		case NOTIFICATION_THEME_CHANGED: {
 			pin_button->set_button_icon(get_editor_theme_icon(SNAME("Pin")));
 			expand_button->set_button_icon(get_editor_theme_icon(SNAME("ExpandBottomDock")));
@@ -103,7 +100,7 @@ void EditorBottomPanel::_repaint() {
 	if (panel_collapsed && get_popup()) {
 		set_popup(nullptr);
 	} else if (!panel_collapsed && !get_popup()) {
-		set_popup(layout_popup);
+		set_popup(dock_context_popup);
 	}
 	if (!panel_collapsed && (previous_tab != -1)) {
 		return;
@@ -123,6 +120,32 @@ void EditorBottomPanel::_repaint() {
 	}
 
 	_theme_changed();
+}
+
+void EditorBottomPanel::dock_closed(EditorDock *p_dock) {
+	if (p_dock == get_current_tab_control()) {
+		hide_bottom_panel();
+	}
+}
+
+void EditorBottomPanel::dock_focused(EditorDock *p_dock, bool p_was_visible) {
+	if (p_was_visible && p_dock->is_visible()) {
+		hide_bottom_panel();
+	}
+}
+
+DockTabContainer::TabStyle EditorBottomPanel::get_tab_style() const {
+	return (TabStyle)EDITOR_GET("interface/editor/bottom_dock_tab_style").operator int();
+}
+
+void EditorBottomPanel::load_selected_tab(int p_idx) {
+	EditorDock *selected_dock = get_dock(p_idx);
+	if (!selected_dock) {
+		p_idx = -1;
+	}
+	set_block_signals(true);
+	set_current_tab(p_idx);
+	set_block_signals(false);
 }
 
 void EditorBottomPanel::save_layout_to_config(Ref<ConfigFile> p_config_file, const String &p_section) const {
@@ -248,10 +271,14 @@ void EditorBottomPanel::_on_button_visibility_changed(Button *p_button, EditorDo
 	}
 }
 
-EditorBottomPanel::EditorBottomPanel() {
+EditorBottomPanel::EditorBottomPanel() :
+		DockTabContainer(DockConstants::DOCK_SLOT_BOTTOM) {
+	layout = DockConstants::DOCK_LAYOUT_HORIZONTAL;
+
 	get_tab_bar()->connect("tab_changed", callable_mp(this, &EditorBottomPanel::_on_tab_changed));
 	set_tabs_position(TabPosition::POSITION_BOTTOM);
 	set_deselect_enabled(true);
+	set_theme_type_variation("BottomPanel");
 
 	bottom_hbox = memnew(HBoxContainer);
 	bottom_hbox->set_mouse_filter(MOUSE_FILTER_IGNORE);

rivie13/Phoenix-Agentic-Engine

editor/docks/editor_dock_manager.h
@@ -73,6 +73,7 @@ public:
 
 class DockContextPopup;
 class EditorDockDragHint;
+class DockTabContainer;
 
 class EditorDockManager : public Object {
 	GDCLASS(EditorDockManager, Object);
@@ -88,13 +89,7 @@ private:
 	Vector<DockSplitContainer *> vsplits;
 	DockSplitContainer *main_hsplit = nullptr;
 
-	struct DockSlot {
-		TabContainer *container = nullptr;
-		EditorDockDragHint *drag_hint = nullptr;
-		EditorDock::DockLayout layout = EditorDock::DOCK_LAYOUT_VERTICAL;
-	};
-
-	DockSlot dock_slots[EditorDock::DOCK_SLOT_MAX];
+	DockTabContainer *dock_slots[EditorDock::DOCK_SLOT_MAX];
 	Vector<WindowWrapper *> dock_windows;
 	LocalVector<EditorDock *> all_docks;
 	HashSet<EditorDock *> dirty_docks;
@@ -110,8 +105,6 @@ private:
 	EditorDock *_get_dock_tab_dragged();
 	void _dock_drag_stopped();
 	void _dock_split_dragged(int p_offset);
-	void _dock_container_popup(int p_tab_idx, TabContainer *p_dock_container);
-	void _dock_container_update_visibility(TabContainer *p_dock_container);
 	void _update_layout();
 
 	void _docks_menu_option(int p_id);
@@ -122,12 +115,10 @@ private:
 	void _restore_dock_to_saved_window(EditorDock *p_dock, const Dictionary &p_window_dump);
 
 	void _make_dock_visible(EditorDock *p_dock, bool p_grab_focus);
-	void _move_dock_tab_index(EditorDock *p_dock, int p_tab_index, bool p_set_current);
 	void _move_dock(EditorDock *p_dock, Control *p_target, int p_tab_index = -1, bool p_set_current = true);
 
 	void _queue_update_tab_style(EditorDock *p_dock);
 	void _update_dirty_dock_tabs();
-	void _update_tab_style(EditorDock *p_dock);
 
 public:
 	static EditorDockManager *get_singleton() { return singleton; }
@@ -138,7 +129,7 @@ public:
 
 	void add_vsplit(DockSplitContainer *p_split);
 	void set_hsplit(DockSplitContainer *p_split);
-	void register_dock_slot(EditorDock::DockSlot p_dock_slot, TabContainer *p_tab_container, EditorDock::DockLayout p_layout);
+	void register_dock_slot(DockTabContainer *p_tab_container);
 	int get_vsplit_count() const;
 	PopupMenu *get_docks_menu();
 
@@ -151,8 +142,6 @@ public:
 	void focus_dock(EditorDock *p_dock);
 	void make_dock_floating(EditorDock *p_dock);
 
-	TabContainer *get_dock_tab_container(Control *p_dock) const;
-
 	void set_docks_visible(bool p_show);
 	bool are_docks_visible() const;
 
@@ -160,36 +149,6 @@ public:
 	void remove_dock(EditorDock *p_dock);
 
 	EditorDockManager();
-};
-
-class EditorDockDragHint : public Control {
-	GDCLASS(EditorDockDragHint, Control);
-
-private:
-	EditorDockManager *dock_manager = nullptr;
-	EditorDock::DockSlot occupied_slot = EditorDock::DOCK_SLOT_MAX;
-	TabBar *drop_tabbar = nullptr;
-
-	Color valid_drop_color;
-	Ref<StyleBoxFlat> dock_drop_highlight;
-	bool can_drop_dock = false;
-	bool mouse_inside = false;
-	bool mouse_inside_tabbar = false;
-
-	void _drag_move_tab(int p_from_index, int p_to_index);
-	void _drag_move_tab_from(TabBar *p_from_tabbar, int p_from_index, int p_to_index);
-
-protected:
-	virtual void gui_input(const Ref<InputEvent> &p_event) override;
-
-	void _notification(int p_what);
-	bool can_drop_data(const Point2 &p_point, const Variant &p_data) const override;
-	void drop_data(const Point2 &p_point, const Variant &p_data) override;
-
-public:
-	void set_slot(EditorDock::DockSlot p_slot);
-
-	EditorDockDragHint();
 };
 
 class DockContextPopup : public PopupPanel {
@@ -215,7 +174,6 @@ private:
 	void _tab_move_right();
 	void _close_dock();
 	void _float_dock();
-	bool _is_slot_available(int p_slot) const;
 
 	void _dock_select_input(const Ref<InputEvent> &p_input);
 	void _dock_select_mouse_exited();
@@ -227,9 +185,7 @@ protected:
 	void _notification(int p_what);
 
 public:
-	void select_current_dock_in_dock_slot(int p_dock_slot);
 	void set_dock(EditorDock *p_dock);
-	EditorDock *get_dock() const;
 	void docks_updated();
 
 	DockContextPopup();

editor/editor_node.cpp
@@ -1460,7 +1460,7 @@ void EditorNode::_sources_changed(bool p_exist) {
 			if (SceneTreeDock::get_singleton()->is_visible_in_tree()) {
 				SceneTreeDock::get_singleton()->get_tree_editor()->get_scene_tree()->grab_focus();
 			} else {
-				TabContainer *tab_container = EditorDockManager::get_singleton()->get_dock_tab_container(SceneTreeDock::get_singleton());
+				TabContainer *tab_container = SceneTreeDock::get_singleton()->get_parent_container();
 				if (tab_container) {
 					// Another tab is active (e.g., Import) - focus the tab bar so user can switch.
 					tab_container->get_tab_bar()->grab_focus();
@@ -8518,24 +8518,36 @@ EditorNode::EditorNode() {
 	left_l_vsplit->set_vertical(true);
 	main_hsplit->add_child(left_l_vsplit);
 
-	TabContainer *dock_slot[EditorDock::DOCK_SLOT_MAX];
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_UL] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_UL]->set_name("DockSlotLeftUL");
-	left_l_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_LEFT_UL]);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_BL] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_BL]->set_name("DockSlotLeftBL");
-	left_l_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_LEFT_BL]);
+	DockTabContainer *dock_slots[EditorDock::DOCK_SLOT_MAX];
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_LEFT_UL));
+		dock_container->set_name("DockSlotLeftUL");
+		left_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_LEFT_BL));
+		dock_container->set_name("DockSlotLeftBL");
+		left_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	left_r_vsplit = memnew(DockSplitContainer);
 	left_r_vsplit->set_name("DockVSplitLeftR");
 	left_r_vsplit->set_vertical(true);
 	main_hsplit->add_child(left_r_vsplit);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_UR] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_UR]->set_name("DockSlotLeftUR");
-	left_r_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_LEFT_UR]);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_BR] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_LEFT_BR]->set_name("DockSlotLeftBR");
-	left_r_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_LEFT_BR]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_LEFT_UR));
+		dock_container->set_name("DockSlotLeftUR");
+		left_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_LEFT_BR));
+		dock_container->set_name("DockSlotLeftBR");
+		left_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	VBoxContainer *center_vb = memnew(VBoxContainer);
 	center_vb->set_h_size_flags(Control::SIZE_EXPAND_FILL);
@@ -8553,23 +8565,35 @@ EditorNode::EditorNode() {
 	right_l_vsplit->set_name("DockVSplitRightL");
 	right_l_vsplit->set_vertical(true);
 	main_hsplit->add_child(right_l_vsplit);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_UL] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_UL]->set_name("DockSlotRightUL");
-	right_l_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_RIGHT_UL]);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_BL] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_BL]->set_name("DockSlotRightBL");
-	right_l_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_RIGHT_BL]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_RIGHT_UL));
+		dock_container->set_name("DockSlotRightUL");
+		right_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_RIGHT_BL));
+		dock_container->set_name("DockSlotRightBL");
+		right_l_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	right_r_vsplit = memnew(DockSplitContainer);
 	right_r_vsplit->set_name("DockVSplitRightR");
 	right_r_vsplit->set_vertical(true);
 	main_hsplit->add_child(right_r_vsplit);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_UR] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_UR]->set_name("DockSlotRightUR");
-	right_r_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_RIGHT_UR]);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_BR] = memnew(TabContainer);
-	dock_slot[EditorDock::DOCK_SLOT_RIGHT_BR]->set_name("DockSlotRightBR");
-	right_r_vsplit->add_child(dock_slot[EditorDock::DOCK_SLOT_RIGHT_BR]);
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_RIGHT_UR));
+		dock_container->set_name("DockSlotRightUR");
+		right_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
+	{
+		DockTabContainer *dock_container = memnew(SideDockTabContainer(EditorDock::DOCK_SLOT_RIGHT_BR));
+		dock_container->set_name("DockSlotRightBR");
+		right_r_vsplit->add_child(dock_container);
+		dock_slots[dock_container->dock_slot] = dock_container;
+	}
 
 	editor_dock_manager = memnew(EditorDockManager);
 
@@ -8582,7 +8606,7 @@ EditorNode::EditorNode() {
 	editor_dock_manager->set_hsplit(main_hsplit);
 
 	for (int i = 0; i < EditorDock::DOCK_SLOT_BOTTOM; i++) {
-		editor_dock_manager->register_dock_slot((EditorDock::DockSlot)i, dock_slot[i], EditorDock::DOCK_LAYOUT_VERTICAL);
+		editor_dock_manager->register_dock_slot(dock_slots[i]);
 	}
 
 	editor_layout_save_delay_timer = memnew(Timer);
@@ -8988,8 +9012,7 @@ EditorNode::EditorNode() {
 	// Bottom panels.
 
 	bottom_panel = memnew(EditorBottomPanel);
-	editor_dock_manager->register_dock_slot(EditorDock::DOCK_SLOT_BOTTOM, bottom_panel, EditorDock::DOCK_LAYOUT_HORIZONTAL);
-	bottom_panel->set_theme_type_variation("BottomPanel");
+	editor_dock_manager->register_dock_slot(bottom_panel);
 	center_split->add_child(bottom_panel);
 	center_split->set_dragger_visibility(SplitContainer::DRAGGER_HIDDEN);
 

editor/gui/editor_bottom_panel.cpp
@@ -39,6 +39,7 @@
 #include "editor/gui/editor_version_button.h"
 #include "editor/scene/editor_scene_tabs.h"
 #include "editor/settings/editor_command_palette.h"
+#include "editor/settings/editor_settings.h"
 #include "scene/gui/box_container.h"
 #include "scene/gui/button.h"
 #include "scene/gui/separator.h"
@@ -48,7 +49,6 @@ void EditorBottomPanel::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_READY: {
 			set_accessibility_region(true);
-			layout_popup = get_popup();
 		} break;
 
 		case NOTIFICATION_THEME_CHANGED: {
@@ -110,7 +110,7 @@ void EditorBottomPanel::_repaint() {
 	if (panel_collapsed && get_popup()) {
 		set_popup(nullptr);
 	} else if (!panel_collapsed && !get_popup()) {
-		set_popup(layout_popup);
+		set_popup(dock_context_popup);
 	}
 	if (!panel_collapsed && (previous_tab != -1)) {
 		return;
@@ -130,6 +130,36 @@ void EditorBottomPanel::_repaint() {
 	} else {
 		_theme_changed();
 	}
+}
+
+void EditorBottomPanel::dock_closed(EditorDock *p_dock) {
+	if (p_dock == get_current_tab_control()) {
+		hide_bottom_panel();
+	}
+}
+
+void EditorBottomPanel::dock_focused(EditorDock *p_dock, bool p_was_visible) {
+	if (p_was_visible && p_dock->is_visible()) {
+		hide_bottom_panel();
+	}
+}
+
+DockTabContainer::TabStyle EditorBottomPanel::get_tab_style() const {
+	return (TabStyle)EDITOR_GET("interface/editor/bottom_dock_tab_style").operator int();
+}
+
+bool EditorBottomPanel::can_switch_dock() const {
+	return !is_locked();
+}
+
+void EditorBottomPanel::load_selected_tab(int p_idx) {
+	EditorDock *selected_dock = get_dock(p_idx);
+	if (!selected_dock) {
+		p_idx = -1;
+	}
+	set_block_signals(true);
+	set_current_tab(p_idx);
+	set_block_signals(false);
 }
 
 void EditorBottomPanel::save_layout_to_config(Ref<ConfigFile> p_config_file, const String &p_section) const {
@@ -251,10 +281,14 @@ void EditorBottomPanel::_on_button_visibility_changed(Button *p_button, EditorDo
 	}
 }
 
-EditorBottomPanel::EditorBottomPanel() {
+EditorBottomPanel::EditorBottomPanel() :
+		DockTabContainer(EditorDock::DOCK_SLOT_BOTTOM) {
+	layout = EditorDock::DOCK_LAYOUT_HORIZONTAL;
+
 	get_tab_bar()->connect("tab_changed", callable_mp(this, &EditorBottomPanel::_on_tab_changed));
 	set_tabs_position(TabPosition::POSITION_BOTTOM);
 	set_deselect_enabled(true);
+	set_theme_type_variation("BottomPanel");
 
 	bottom_hbox = memnew(HBoxContainer);
 	bottom_hbox->set_mouse_filter(MOUSE_FILTER_IGNORE);