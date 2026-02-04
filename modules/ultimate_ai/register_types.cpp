1255f04af2c4e67ea8d25d0b58231677d88174e1
rivie13 <riviera.t.sperduto13@gmail.com>
2026-02-04 17:19:01 -0500

    fix: standardize indentation in ultimate_ai module initialization functions

--- modules/ultimate_ai/register_types.cpp
+++ modules/ultimate_ai/register_types.cpp
@@ -31,17 +31,17 @@
 #include "register_types.h"
 
 void initialize_ultimate_ai_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
-        return;
-    }
+	if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		return;
+	}
 
-    // TODO: Register classes for the Ultimate AI module.
+	// TODO: Register classes for the Ultimate AI module.
 }
 
 void uninitialize_ultimate_ai_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
-        return;
-    }
+	if (p_level != MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		return;
+	}
 
-    // TODO: Unregister classes for the Ultimate AI module.
+	// TODO: Unregister classes for the Ultimate AI module.
 }
